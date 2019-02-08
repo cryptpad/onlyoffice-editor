201a0c80d7e8f0651903669b2530b9425492e389
Ilya Kirillov <Ilya.Kirillov@onlyoffice.com>
2019-02-08 18:49:10 +0300

    Add the function to check if the comment is quoted

--- word/Editor/Comments.js
+++ word/Editor/Comments.js
@@ -491,6 +491,10 @@ CComment.prototype.IsSolved = function()
 
 	return false;
 };
+CComment.prototype.IsQuoted = function()
+{
+	return (this.Data && null !== this.Data.GetQuoteText());
+};
 
 var comments_NoComment        = 0;
 var comments_NonActiveComment = 1;
