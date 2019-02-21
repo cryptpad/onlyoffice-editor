a140ddcf34b2acf8e9f2d3db6f6f8b3a4264fdae
GoshaZotov <Igor.Zotov@onlyoffice.com>
2019-02-21 15:28:48 +0300

    [se] Fix bug 40680

--- cell/view/WorksheetView.js
+++ cell/view/WorksheetView.js
@@ -10556,19 +10556,21 @@
 								arrayFormulaRef = arrayFormulaRef.intersection(formulaProps.fromRange);
 							}
 
-							if (specialPasteProps.transpose) {
-								var diffCol1 = arrayFormulaRef.c1 - activeCellsPasteFragment.c1;
-								var diffRow1 = arrayFormulaRef.r1 - activeCellsPasteFragment.r1;
-								var diffCol2 = arrayFormulaRef.c2 - activeCellsPasteFragment.c1;
-								var diffRow2 = arrayFormulaRef.r2 - activeCellsPasteFragment.r1;
+							if(arrayFormulaRef) {
+								if (specialPasteProps.transpose) {
+									var diffCol1 = arrayFormulaRef.c1 - activeCellsPasteFragment.c1;
+									var diffRow1 = arrayFormulaRef.r1 - activeCellsPasteFragment.r1;
+									var diffCol2 = arrayFormulaRef.c2 - activeCellsPasteFragment.c1;
+									var diffRow2 = arrayFormulaRef.r2 - activeCellsPasteFragment.r1;
 
-								arrayFormulaRef.c1 = activeCellsPasteFragment.c1 + diffRow1;
-								arrayFormulaRef.r1 = activeCellsPasteFragment.r1 + diffCol1;
-								arrayFormulaRef.c2 = activeCellsPasteFragment.c1 + diffRow2;
-								arrayFormulaRef.r2 = activeCellsPasteFragment.r1 + diffCol2;
+									arrayFormulaRef.c1 = activeCellsPasteFragment.c1 + diffRow1;
+									arrayFormulaRef.r1 = activeCellsPasteFragment.r1 + diffCol1;
+									arrayFormulaRef.c2 = activeCellsPasteFragment.c1 + diffRow2;
+									arrayFormulaRef.r2 = activeCellsPasteFragment.r1 + diffCol2;
+								}
+
+								arrayFormulaRef.setOffset(offset);
 							}
-
-							arrayFormulaRef.setOffset(offset);
 						}
 						if(specialPasteProps.transpose)
 						{
