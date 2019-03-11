4ff7f4024a6269896c29f89feb42c572476f6251
Ilya Kirillov <Ilya.Kirillov@onlyoffice.com>
2019-03-11 13:47:48 +0300

    Add the first variant of calculating and rendering for the reviewed numbering

--- word/Editor/ParagraphContent.js
+++ word/Editor/ParagraphContent.js
@@ -1210,38 +1210,95 @@ function ParaNumbering()
 	this.Page  = 0;
 
 	this.Internal = {
-		NumInfo   : undefined,
-		CalcValue : -1
+		FinalNumInfo    : undefined,
+		FinalCalcValue  : -1,
+		FinalNumId      : null,
+		FinalNumLvl     : -1,
+
+		SourceNumInfo   : undefined,
+		SourceCalcValue : -1,
+		SourceNumId     : null,
+		SourceNumLvl    : -1,
+		SourceWidth     : 0
 	};
 }
 ParaNumbering.prototype = Object.create(CRunElementBase.prototype);
 ParaNumbering.prototype.constructor = ParaNumbering;
 
 ParaNumbering.prototype.Type = para_Numbering;
-ParaNumbering.prototype.Draw = function(X,Y,Context, Numbering, TextPr, NumPr, Theme)
+ParaNumbering.prototype.Draw = function(X, Y, oContext, oNumbering, oTextPr, oTheme)
 {
-	Numbering.Draw(NumPr.NumId, NumPr.Lvl, X, Y, Context, this.Internal.NumInfo, TextPr, Theme);
-};
-ParaNumbering.prototype.Measure = function (Context, Numbering, NumInfo, TextPr, NumPr, Theme)
-{
-	this.Internal.NumInfo   = NumInfo;
-	this.Internal.CalcValue = NumInfo && NumPr ? NumInfo[NumPr.Lvl] : -1;
+	var _X = X;
+	var oTempTextPr = oTextPr.Copy();
+	if (this.Internal.SourceNumInfo)
+	{
+		oTempTextPr.Strikeout = true;
+		oTempTextPr.Color = new CDocumentColor(255, 0, 0);
 
+		oNumbering.Draw(this.Internal.SourceNumId,this.Internal.SourceNumLvl, _X, Y, oContext, this.Internal.SourceNumInfo, oTempTextPr, oTheme);
+
+		_X += this.Internal.SourceWidth;
+	}
+
+	if (this.Internal.FinalNumInfo)
+	{
+		if (this.Internal.SourceNumInfo)
+			oTempTextPr.Color = new CDocumentColor(255, 0, 0);
+
+		oNumbering.Draw(this.Internal.FinalNumId,this.Internal.FinalNumLvl, _X, Y, oContext, this.Internal.FinalNumInfo, oTempTextPr, oTheme);
+	}
+};
+ParaNumbering.prototype.Measure = function (oContext, oNumbering, oTextPr, oTheme, oFinalNumInfo, oFinalNumPr, oSourceNumInfo, oSourceNumPr)
+{
 	this.Width        = 0;
 	this.Height       = 0;
 	this.WidthVisible = 0;
 	this.WidthNum     = 0;
 	this.WidthSuff    = 0;
 
-	if (undefined === Numbering)
-		return {Width : this.Width, Height : this.Height, WidthVisible : this.WidthVisible};
+	if (!oNumbering)
+	{
+		return {
+			Width        : this.Width,
+			Height       : this.Height,
+			WidthVisible : this.WidthVisible
+		}
+	}
 
-	var Temp          = Numbering.Measure(NumPr.NumId, NumPr.Lvl, Context, NumInfo, TextPr, Theme);
-	this.Width        = Temp.Width;
-	this.WidthVisible = Temp.Width;
-	this.WidthNum     = Temp.Width;
+	var nWidth = 0, nAscent = 0;
+	if (oFinalNumInfo && oFinalNumPr)
+	{
+		var oTemp = oNumbering.Measure(oFinalNumPr.NumId, oFinalNumPr.Lvl, oContext, oFinalNumInfo, oTextPr, oTheme);
+
+		this.Internal.FinalNumInfo   = oFinalNumInfo;
+		this.Internal.FinalCalcValue = oFinalNumInfo[oFinalNumPr.Lvl];
+		this.Internal.FinalNumId     = oFinalNumPr.NumId;
+		this.Internal.FinalNumLvl    = oFinalNumPr.Lvl;
+
+		nWidth    = oTemp.Width;
+		nAscent   = oTemp.Ascent;
+	}
+
+	if (oSourceNumInfo && oSourceNumPr)
+	{
+		var oTemp = oNumbering.Measure(oSourceNumPr.NumId, oSourceNumPr.Lvl, oContext, oSourceNumInfo, oTextPr, oTheme);
+
+		this.Internal.SourceNumInfo   = oSourceNumInfo;
+		this.Internal.SourceCalcValue = oSourceNumInfo[oSourceNumPr.Lvl];
+		this.Internal.SourceNumId     = oSourceNumPr.NumId;
+		this.Internal.SourceNumLvl    = oSourceNumPr.Lvl;
+		this.Internal.SourceWidth     = oTemp.Width;
+		nWidth += this.Internal.SourceWidth;
+
+		if (nAscent < oTemp.Ascent)
+			nAscent = oTemp.Ascent;
+	}
+
+	this.Width        = nWidth;
+	this.WidthVisible = nWidth;
+	this.WidthNum     = nWidth;
 	this.WidthSuff    = 0;
-	this.Height       = Temp.Ascent; // Это не вся высота, а только высота над BaseLine
+	this.Height       = nAscent; // Это не вся высота, а только высота над BaseLine
 };
 ParaNumbering.prototype.Check_Range = function(Range, Line)
 {
@@ -1272,7 +1329,7 @@ ParaNumbering.prototype.Read_FromBinary = function(Reader)
 };
 ParaNumbering.prototype.GetCalculatedValue = function()
 {
-	return this.Internal.CalcValue;
+	return this.Internal.FinalCalcValue;
 };
 
 // TODO: Реализовать табы по точке и с чертой.
