d4dd36ff03fae31ccfe2ec7e84d92c9e77121ebd
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2019-06-24 20:33:39 +0300

    [se] Export
    Export function getCellIndex

--- cell/model/Workbook.js
+++ cell/model/Workbook.js
@@ -12951,6 +12951,7 @@
 	window['AscCommonExcel'].aStandartNumFormatsId = aStandartNumFormatsId;
 	window['AscCommonExcel'].oFormulaLocaleInfo = oFormulaLocaleInfo;
 	window['AscCommonExcel'].getDefNameIndex = getDefNameIndex;
+	window['AscCommonExcel'].getCellIndex = getCellIndex;
 	window['AscCommonExcel'].angleFormatToInterface2 = angleFormatToInterface2;
 	window['AscCommonExcel'].angleInterfaceToFormat = angleInterfaceToFormat;
 	window['AscCommonExcel'].Workbook = Workbook;
