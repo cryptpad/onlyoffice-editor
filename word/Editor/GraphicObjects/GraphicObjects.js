b06a74c494f717b78fb4ca7f796a1ef62b585221
Sergey Luzyanin <Sergey.Luzyanin@onlyoffice.com>
2019-08-07 11:29:23 +0300

    [de] Fix bug 42317

--- word/Editor/GraphicObjects/GraphicObjects.js
+++ word/Editor/GraphicObjects/GraphicObjects.js
@@ -730,7 +730,8 @@ CGraphicObjects.prototype =
         if(AscFormat.isRealNumber(oApplyProps.Width) || AscFormat.isRealNumber(oApplyProps.Height) || AscFormat.isRealNumber(oApplyProps.rot)
             || AscFormat.isRealNumber(oApplyProps.rotAdd)
             || AscFormat.isRealBool(oApplyProps.flipH)|| AscFormat.isRealBool(oApplyProps.flipV)
-            || AscFormat.isRealBool(oApplyProps.flipHInvert)|| AscFormat.isRealBool(oApplyProps.flipVInvert) || (typeof oApplyProps.type === "string"))
+            || AscFormat.isRealBool(oApplyProps.flipHInvert)|| AscFormat.isRealBool(oApplyProps.flipVInvert) || (typeof oApplyProps.type === "string")
+            || AscCommon.isRealObject(oApplyProps.shadow) || oApplyProps.shadow === null)
         {
             /*в случае если в насторойках ParaDrawing стоит UseAlign - пересчитываем drawing, т. к. ширина и высото ParaDrawing рассчитывается по bounds*/
             var aSelectedObjects = this.selectedObjects;
