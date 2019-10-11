f44260c25c6c868801bd15143419271f5b58a623
Ilya Kirillov <Ilya.Kirillov@onlyoffice.com>
2019-10-11 14:42:13 +0300

    Fix previous commit

--- word/apiDefines.js
+++ word/apiDefines.js
@@ -280,11 +280,6 @@ window['AscCommon']                = window['AscCommon'] || {};
 window['AscCommon'].c_oSerFormat   = c_oSerFormat;
 window['AscCommon'].CurFileVersion = c_oSerFormat.Version;
 
-prot = window['Asc']['c_oAscWatermarkType'] = window['Asc'].c_oAscWatermarkType = c_oAscWatermarkType;
-prot['None'] = prot.None;
-prot['Text'] = prot.Text;
-prot['Image'] = prot.Image;
-
 prot = window['Asc']['c_oAscSdtCheckBoxDefaults'] = window['Asc'].c_oAscSdtCheckBoxDefaults = c_oAscSdtCheckBoxDefaults;
 prot['CheckedSymbol']   = prot.CheckedSymbol;
 prot['UncheckedSymbol'] = prot.UncheckedSymbol;
