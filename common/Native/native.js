cbb3544cf54c4dbee60169e15e3bdb7d0a2e3ece
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2019-10-14 18:03:28 +0300

    Deleted all unused code

--- common/Native/native.js
+++ common/Native/native.js
@@ -75,24 +75,11 @@ var AscCommonWord = window["AscCommonWord"];
 window["AscCommonSlide"] = {};
 var AscCommonSlide = window["AscCommonSlide"];
 
-function ConvertJSC_Array(_array)
-{
-	var _len = _array.length;
-	var ret = new Uint8Array(_len);
-	for (var i = 0; i < _len; i++)
-		ret[i] = _array.getAt(i);
-	return ret;
-}
-
 function Image()
 {
 	this.src = "";
-	this.onload = function ()
-	{
-	};
-	this.onerror = function ()
-	{
-	};
+	this.onload = function () {};
+	this.onerror = function () {};
 }
 
 function _image_data()
@@ -106,19 +93,13 @@ function native_pattern_fill()
 }
 native_pattern_fill.prototype =
 {
-	setTransform: function (transform)
-	{
-	}
+	setTransform: function (transform) {}
 };
 
-function native_gradient_fill()
-{
-}
+function native_gradient_fill() {}
 native_gradient_fill.prototype =
 {
-	addColorStop: function (offset, color)
-	{
-	}
+	addColorStop: function (offset, color) {}
 };
 
 function native_context2d(parent)
@@ -144,109 +125,44 @@ function native_context2d(parent)
 }
 native_context2d.prototype =
 {
-	save: function ()
-	{
-	},
-	restore: function ()
-	{
-	},
+	save: function () {},
+	restore: function () {},
 
-	scale: function (x, y)
-	{
-	},
-	rotate: function (angle)
-	{
-	},
-	translate: function (x, y)
-	{
-	},
-	transform: function (m11, m12, m21, m22, dx, dy)
-	{
-	},
-	setTransform: function (m11, m12, m21, m22, dx, dy)
-	{
-	},
+	scale: function (x, y) {},
+	rotate: function (angle) {},
+	translate: function (x, y) {},
+	transform: function (m11, m12, m21, m22, dx, dy) {},
+	setTransform: function (m11, m12, m21, m22, dx, dy) {},
 
-	createLinearGradient: function (x0, y0, x1, y1)
-	{
-		return new native_gradient_fill();
-	},
-	createRadialGradient: function (x0, y0, r0, x1, y1, r1)
-	{
-		return null;
-	},
-	createPattern: function (image, repetition)
-	{
-		return new native_pattern_fill();
-	},
+	createLinearGradient: function (x0, y0, x1, y1) {},
+	createRadialGradient: function (x0, y0, r0, x1, y1, r1) {},
+	createPattern: function (image, repetition) {},
 
-	clearRect: function (x, y, w, h)
-	{
-	},
-	fillRect: function (x, y, w, h)
-	{
-	},
-	strokeRect: function (x, y, w, h)
-	{
-	},
+	clearRect: function (x, y, w, h) {},
+	fillRect: function (x, y, w, h) {},
+	strokeRect: function (x, y, w, h) {},
 
-	beginPath: function ()
-	{
-	},
-	closePath: function ()
-	{
-	},
-	moveTo: function (x, y)
-	{
-	},
-	lineTo: function (x, y)
-	{
-	},
-	quadraticCurveTo: function (cpx, cpy, x, y)
-	{
-	},
-	bezierCurveTo: function (cp1x, cp1y, cp2x, cp2y, x, y)
-	{
-	},
-	arcTo: function (x1, y1, x2, y2, radius)
-	{
-	},
-	rect: function (x, y, w, h)
-	{
-	},
-	arc: function (x, y, radius, startAngle, endAngle, anticlockwise)
-	{
-	},
+	beginPath: function () {},
+	closePath: function () {},
+	moveTo: function (x, y) {},
+	lineTo: function (x, y) {},
+	quadraticCurveTo: function (cpx, cpy, x, y) {},
+	bezierCurveTo: function (cp1x, cp1y, cp2x, cp2y, x, y) {},
+	arcTo: function (x1, y1, x2, y2, radius) {},
+	rect: function (x, y, w, h) {},
+	arc: function (x, y, radius, startAngle, endAngle, anticlockwise) {},
 
-	fill: function ()
-	{
-	},
-	stroke: function ()
-	{
-	},
-	clip: function ()
-	{
-	},
-	isPointInPath: function (x, y)
-	{
-	},
-	drawFocusRing: function (element, xCaret, yCaret, canDrawCustom)
-	{
-	},
+	fill: function () {},
+	stroke: function () {},
+	clip: function () {},
+	isPointInPath: function (x, y) {},
+	drawFocusRing: function (element, xCaret, yCaret, canDrawCustom) {},
 
-	fillText: function (text, x, y, maxWidth)
-	{
-	},
-	strokeText: function (text, x, y, maxWidth)
-	{
-	},
-	measureText: function (text)
-	{
-	},
+	fillText: function (text, x, y, maxWidth) {},
+	strokeText: function (text, x, y, maxWidth) {},
+	measureText: function (text) {},
 
-	drawImage: function (img_elem, dx_or_sx, dy_or_sy, dw_or_sw, dh_or_sh, dx, dy, dw, dh)
-	{
-	},
+	drawImage: function (img_elem, dx_or_sx, dy_or_sy, dw_or_sw, dh_or_sh, dx, dy, dw, dh) {},
 
 	createImageData: function (imagedata_or_sw, sh)
 	{
@@ -255,12 +171,8 @@ native_context2d.prototype =
 		_data.data = (typeof(Uint8Array) != 'undefined') ? new Uint8Array(_data.length) : new Array(_data.length);
 		return _data;
 	},
-	getImageData: function (sx, sy, sw, sh)
-	{
-	},
-	putImageData: function (image_data, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight)
-	{
-	}
+	getImageData: function (sx, sy, sw, sh) {},
+	putImageData: function (image_data, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight) {}
 };
 
 function native_canvas()
@@ -273,25 +185,10 @@ function native_canvas()
 }
 native_canvas.prototype =
 {
-	getContext: function (type)
-	{
-		if (type == "2d")
-			return new native_context2d(this);
-		return null;
-	},
-
-	toDataUrl: function (type)
-	{
-		return "";
-	},
-
-	addEventListener: function ()
-	{
-	},
-
-	attr: function ()
-	{
-	}
+	getContext: function (type) { return (type == "2d") ? new native_context2d(this) : null; },
+	toDataUrl: function (type) { return ""; },
+	addEventListener: function () {},
+	attr: function () {}
 };
 
 var _null_object = {};
@@ -309,73 +206,26 @@ _null_object.body = _null_object;
 _null_object.ownerDocument = _null_object;
 _null_object.defaultView = _null_object;
 
-_null_object.addEventListener = function ()
-{
-};
-_null_object.setAttribute = function ()
-{
-};
-_null_object.getElementsByTagName = function ()
-{
-	return [];
-};
-_null_object.appendChild = function ()
-{
-};
-_null_object.removeChild = function ()
-{
-};
-_null_object.insertBefore = function ()
-{
-};
+_null_object.addEventListener = function () {};
+_null_object.setAttribute = function () {};
+_null_object.getElementsByTagName = function () { return []; };
+_null_object.appendChild = function () {};
+_null_object.removeChild = function () {};
+_null_object.insertBefore = function () {};
 _null_object.childNodes = [];
 _null_object.parent = _null_object;
 _null_object.parentNode = _null_object;
-_null_object.find = function ()
-{
-	return this;
-};
-_null_object.appendTo = function ()
-{
-	return this;
-};
-_null_object.css = function ()
-{
-	return this;
-};
-_null_object.width = function ()
-{
-	return 0;
-};
-_null_object.height = function ()
-{
-	return 0;
-};
-_null_object.attr = function ()
-{
-	return this;
-};
-_null_object.prop = function ()
-{
-	return this;
-};
-_null_object.val = function ()
-{
-	return this;
-};
-_null_object.remove = function ()
-{
-};
-_null_object.getComputedStyle = function ()
-{
-	return null;
-};
-_null_object.getContext = function (type)
-{
-	if (type == "2d")
-		return new native_context2d(this);
-	return null;
-};
+_null_object.find = function () { return this; };
+_null_object.appendTo = function () { return this; };
+_null_object.css = function () { return this; };
+_null_object.width = function () { return 0; };
+_null_object.height = function () { return 0; };
+_null_object.attr = function () { return this; };
+_null_object.prop = function () { return this; };
+_null_object.val = function () { return this; };
+_null_object.remove = function () {};
+_null_object.getComputedStyle = function () { return null; };
+_null_object.getContext = function (type) { return (type == "2d") ? new native_context2d(this) : null; };
 
 window._null_object = _null_object;
 
@@ -390,443 +240,105 @@ document.createElement = function (type)
 	return _null_object;
 };
 
-function _return_empty_html_element()
-{
-	return _null_object;
-};
+function _return_empty_html_element() { return _null_object; }
 
 document.createDocumentFragment = _return_empty_html_element;
-document.getElementsByTagName = function (tag)
-{
-	var ret = [];
-	if ("head" == tag)
-		ret.push(_null_object);
-	return ret;
-};
-document.insertBefore = function ()
-{
-};
-document.appendChild = function ()
-{
-};
-document.removeChild = function ()
-{
-};
-document.getElementById = function ()
-{
-	return _null_object;
-};
-document.createComment = function ()
-{
-	return undefined;
-};
+document.getElementsByTagName = function (tag) { return ("head" == tag) ? [_null_object] : []; };
+document.insertBefore = function () {};
+document.appendChild = function () {};
+document.removeChild = function () {};
+document.getElementById = function () {	return _null_object; };
+document.createComment = function () { return undefined; };
 
 document.documentElement = _null_object;
 document.body = _null_object;
 
+// NATIVE OBJECT
 var native = (typeof native === undefined) ? undefined : native;
 if (!native)
 {
 	if (typeof NativeEngine === "undefined")
-	{
 		native = CreateNativeEngine();
-	}
 	else
-	{
 		native = NativeEngine;
-	}
 }
 
 window.native = native;
+function GetNativeEngine() { return window.native; }
 
-function GetNativeEngine()
-{
-	return window.native;
-}
+var Api = null; // main builder object
 
-var native_renderer = null;
-
+// OPEN
 function NativeOpenFileData(data, version, xlsx_file_path)
 {
 	window.NATIVE_DOCUMENT_TYPE = window.native.GetEditorType();
-	var _api;
+    Api = null;
 	if (window.NATIVE_DOCUMENT_TYPE == "presentation" || window.NATIVE_DOCUMENT_TYPE == "document")
 	{
-		_api = new window["Asc"]["asc_docs_api"]({});
-		_api.asc_nativeOpenFile(data, version);
+        Api = new window["Asc"]["asc_docs_api"]({});
+        Api.asc_nativeOpenFile(data, version);
 	}
 	else
 	{
-		_api = new window["Asc"]["spreadsheet_api"]({});
-		_api.asc_nativeOpenFile(data, version, undefined, xlsx_file_path);
+        Api = new window["Asc"]["spreadsheet_api"]({});
+        Api.asc_nativeOpenFile(data, version, undefined, xlsx_file_path);
 	}
-	window["_api"] = window["API"] = _api;
 }
 
-function NativeOpenFile()
-{
-	var doc_bin = window.native.GetFileString(window.native.GetFilePath());
-	window.NATIVE_DOCUMENT_TYPE = window.native.GetEditorType();
-
-	var _api;
-	if (window.NATIVE_DOCUMENT_TYPE == "presentation" || window.NATIVE_DOCUMENT_TYPE == "document")
-	{
-		_api = new window["Asc"]["asc_docs_api"]("");
-	}
-	else
-	{
-		_api = new window["Asc"]["spreadsheet_api"]();
-	}
-	_api.asc_nativeOpenFile(doc_bin);
-	window["_api"] = window["API"] = _api;
-}
-
-function NativeOpenFile2(_params)
-{
-	window["CreateMainTextMeasurerWrapper"]();
-
-	window.g_file_path = "native_open_file";
-	window.NATIVE_DOCUMENT_TYPE = window.native.GetEditorType();
-	var doc_bin = window.native.GetFileString(window.g_file_path);
-	var _api;
-	if (window.NATIVE_DOCUMENT_TYPE == "presentation" || window.NATIVE_DOCUMENT_TYPE == "document")
-	{
-		_api = new window["Asc"]["asc_docs_api"]("");
-
-		if (undefined !== _api.Native_Editor_Initialize_Settings)
-		{
-			_api.Native_Editor_Initialize_Settings(_params);
-		}
-
-		_api.asc_nativeOpenFile(doc_bin);
-
-		if (_api.NativeAfterLoad)
-			_api.NativeAfterLoad();
-
-		// ToDo get_PropertyThemeColorSchemes method removed, now the only Event!!!!
-		/*if (_api.__SendThemeColorScheme)
-			_api.__SendThemeColorScheme();
-
-		if (_api.get_PropertyThemeColorSchemes)
-		{
-			var schemes = _api.get_PropertyThemeColorSchemes();
-			if (schemes)
-			{
-				var st = global_memory_stream_menu;
-				st["ClearNoAttack"]();
-				AscCommon.asc_WriteColorSchemes(schemes, st);
-				window["native"]["OnCallMenuEvent"](2404, st); // ASC_MENU_EVENT_TYPE_COLOR_SCHEMES
-			}
-		}*/
-	}
-	else
-	{
-		_api = new window["Asc"]["spreadsheet_api"]();
-		_api.asc_nativeOpenFile(doc_bin);
-	}
-
-	window["_api"] = window["API"] = _api;
-}
-
+// CALCULATE & PAGE COUNT
 function NativeCalculateFile()
 {
-	window["API"].asc_nativeCalculateFile();
+    Api.asc_nativeCalculateFile();
+}
+function GetNativeCountPages()
+{
+    return Api.asc_nativePrintPagesCount();
 }
 
+// APPLY CHANGES
 function NativeApplyChangesData(data, isFull)
 {
-	window["API"].asc_nativeApplyChanges2(data, isFull);
+    Api.asc_nativeApplyChanges2(data, isFull);
 }
 
-function NativeApplyChanges()
-{
-	var __changes = [];
-	var _count_main = window.native.GetCountChanges();
-	for (var i = 0; i < _count_main; i++)
-	{
-		var _changes_file = window.native.GetChangesFile(i);
-		var _changes = JSON.parse(window.native.GetFileString(_changes_file));
-
-		for (var j = 0; j < _changes.length; j++)
-		{
-			__changes.push(_changes[j]);
-		}
-	}
-
-	window["API"].asc_nativeApplyChanges(__changes);
-}
-function NativeGetFileString()
-{
-	return window["API"].asc_nativeGetFile();
-}
+// SAVE TO T-FORMAT
 function NativeGetFileData()
 {
-	return window["API"].asc_nativeGetFileData();
+	return Api.asc_nativeGetFileData();
 }
+// SAVE TO HTML-FORMAT (WORD ONLY)
 function NativeGetFileDataHtml()
 {
-	if (window["API"].asc_nativeGetHtml)
-		return window["API"].asc_nativeGetHtml();
-	return "";
+	return Api.asc_nativeGetHtml ? Api.asc_nativeGetHtml() : "";
+}
+// SAVE TO PDF
+function GetNativeFileDataPDF(_param)
+{
+    return Api.asc_nativeGetPDF(_param);
 }
 
+// MAIL MERGE
 function NativeStartMailMergeByList(database)
 {
-	if (window["API"].asc_StartMailMergeByList)
-		return window["API"].asc_StartMailMergeByList(database);
-	return undefined;
+    return Api.asc_StartMailMergeByList ? Api.asc_StartMailMergeByList(database) : undefined;
 }
 function NativePreviewMailMergeResult(index)
 {
-	if (window["API"].asc_PreviewMailMergeResult)
-		return window["API"].asc_PreviewMailMergeResult(index);
-	return undefined;
+    return Api.asc_PreviewMailMergeResult ? Api.asc_PreviewMailMergeResult(index) : undefined;
 }
 function NativeGetMailMergeFiledValue(index, name)
 {
-	if (window["API"].asc_GetMailMergeFiledValue)
-		return window["API"].asc_GetMailMergeFiledValue(index, name);
-	return "";
+    return Api.asc_GetMailMergeFiledValue ? Api.asc_GetMailMergeFiledValue(index, name) : "";
 }
 
-function GetNativeCountPages()
-{
-	return window["API"].asc_nativePrintPagesCount();
-}
-
-function GetNativeFileDataPDF(_param)
-{
-	return window["API"].asc_nativeGetPDF(_param);
-}
-
-window.memory1 = null;
-window.memory2 = null;
-
-function GetNativePageBase64(pageIndex)
-{
-	if (null == window.memory1)
-		window.memory1 = CreateNativeMemoryStream();
-	else
-		window.memory1.ClearNoAttack();
-
-	if (null == window.memory2)
-		window.memory2 = CreateNativeMemoryStream();
-	else
-		window.memory2.ClearNoAttack();
-
-	if (native_renderer == null)
-	{
-		native_renderer = window["API"].asc_nativeCheckPdfRenderer(window.memory1, window.memory2);
-	}
-	else
-	{
-		window.memory1.ClearNoAttack();
-		window.memory2.ClearNoAttack();
-	}
-
-	window["API"].asc_nativePrint(native_renderer, pageIndex);
-	return window.memory1;
-}
-
-function GetNativePageMeta(pageIndex)
-{
-	return window["API"].GetNativePageMeta(pageIndex);
-}
-
-function GetNativeId()
-{
-	return window.native.GetFileId();
-}
-
-// для работы с таймерами
-window.NativeSupportTimeouts = true;
-window.NativeTimeoutObject = {};
-
-function clearTimeout(id)
-{
-	if (!window.NativeSupportTimeouts)
-		return;
-
-	window.NativeTimeoutObject["" + id] = undefined;
-	window["native"]["ClearTimeout"](id);
-}
-function setTimeout(func, interval)
-{
-	if (!window.NativeSupportTimeouts)
-		return;
-
-	var id = window["native"]["GenerateTimeoutId"](interval);
-	window.NativeTimeoutObject["" + id] = {"func": func, repeat: false};
-	return id;
-}
-
-function clearInterval(id)
-{
-	if (!window.NativeSupportTimeouts)
-		return;
-
-	window.NativeTimeoutObject["" + id] = undefined;
-	window["native"]["ClearTimeout"](id);
-}
-function setInterval(func, interval)
-{
-	if (!window.NativeSupportTimeouts)
-		return;
-
-	var id = window["native"]["GenerateTimeoutId"](interval);
-	window.NativeTimeoutObject["" + id] = {func: func, repeat: true, interval: interval};
-	return id;
-}
-
-window.native.Call_TimeoutFire = function (id)
-{
-	if (!window.NativeSupportTimeouts)
-		return;
-
-	var prop = "" + id;
-
-	var timeoutObject = window.NativeTimeoutObject[prop];
-	if (!timeoutObject) {
-		return;
-	}
-
-	window.NativeTimeoutObject[prop] = undefined;
-
-	if (!timeoutObject.func)
-		return;
-
-	timeoutObject.func.call(null);
-
-	if (timeoutObject.repeat) {
-		setInterval(timeoutObject.func, timeoutObject.interval);
-	}
-
-	timeoutObject.func = null;
-};
-
-window.clearTimeout = clearTimeout;
-window.setTimeout = setTimeout;
-window.clearInterval = clearInterval;
-window.setInterval = setInterval;
+window.clearTimeout = function() {};
+window.setTimeout = function() {};
+window.clearInterval = function() {};
+window.setInterval = function() {};
 
 var console = {
-	log: function (param)
-	{
-		window.native.ConsoleLog(param);
-	},
-	time: function (param)
-	{
-	},
-	timeEnd: function (param)
-	{
-	}
+	log: function (param) { window.native.ConsoleLog(param); },
+	time: function (param) {},
+	timeEnd: function (param) {}
 };
 
-window["NativeCorrectImageUrlOnPaste"] = function (url)
-{
-	return window["native"]["CorrectImageUrlOnPaste"](url);
-};
-window["NativeCorrectImageUrlOnCopy"] = function (url)
-{
-	return window["native"]["CorrectImageUrlOnCopy"](url);
-};
-
-var global_memory_stream_menu = CreateNativeMemoryStream();
-
-// HTML page interface
-window.native.Call_OnUpdateOverlay = function (param)
-{
-	return window["API"].Call_OnUpdateOverlay(param);
-};
-
-window.native.Call_OnMouseDown = function (e)
-{
-	return window["API"].Call_OnMouseDown(e);
-};
-window.native.Call_OnMouseUp = function (e)
-{
-	return window["API"].Call_OnMouseUp(e);
-};
-window.native.Call_OnMouseMove = function (e)
-{
-	return window["API"].Call_OnMouseMove(e);
-};
-window.native.Call_OnCheckMouseDown = function (e)
-{
-	return window["API"].Call_OnCheckMouseDown(e);
-};
-
-window.native.Call_OnKeyDown = function (e)
-{
-	return window["API"].Call_OnKeyDown(e);
-};
-window.native.Call_OnKeyPress = function (e)
-{
-	return window["API"].Call_OnKeyPress(e);
-};
-window.native.Call_OnKeyUp = function (e)
-{
-	return window["API"].Call_OnKeyUp(e);
-};
-window.native.Call_OnKeyboardEvent = function (e)
-{
-	return window["API"].Call_OnKeyboardEvent(e);
-};
-
-window.native.Call_CalculateResume = function ()
-{
-	return window["API"].Call_CalculateResume();
-};
-
-window.native.Call_TurnOffRecalculate = function ()
-{
-	return window["API"].Call_TurnOffRecalculate();
-};
-window.native.Call_TurnOnRecalculate = function ()
-{
-	return window["API"].Call_TurnOnRecalculate();
-};
-
-window.native.Call_CheckTargetUpdate = function ()
-{
-	return window["API"].Call_CheckTargetUpdate();
-};
-window.native.Call_Common = function (type, param)
-{
-	return window["API"].Call_Common(type, param);
-};
-
-window.native.Call_HR_Tabs = function (arrT, arrP)
-{
-	return window["API"].Call_HR_Tabs(arrT, arrP);
-};
-window.native.Call_HR_Pr = function (_indent_left, _indent_right, _indent_first)
-{
-	return window["API"].Call_HR_Pr(_indent_left, _indent_right, _indent_first);
-};
-window.native.Call_HR_Margins = function (_margin_left, _margin_right)
-{
-	return window["API"].Call_HR_Margins(_margin_left, _margin_right);
-};
-window.native.Call_HR_Table = function (_params, _cols, _margins, _rows)
-{
-	return window["API"].Call_HR_Table(_params, _cols, _margins, _rows);
-};
-
-window.native.Call_VR_Margins = function (_top, _bottom)
-{
-	return window["API"].Call_VR_Margins(_top, _bottom);
-};
-window.native.Call_VR_Header = function (_header_top, _header_bottom)
-{
-	return window["API"].Call_VR_Header(_header_top, _header_bottom);
-};
-window.native.Call_VR_Table = function (_params, _cols, _margins, _rows)
-{
-	return window["API"].Call_VR_Table(_params, _cols, _margins, _rows);
-};
-
-window.native.Call_Menu_Event = function (type, _params)
-{
-	return window["API"].Call_Menu_Event(type, _params);
-};
