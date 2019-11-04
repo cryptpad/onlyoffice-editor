111a98dc6018104b803b3ed061076a2049b435fe
SergeyLuzyanin <Sergey.Luzyanin@onlyoffice.com>
2019-11-04 23:02:52 +0300

    [de] Remove drawing behindDoc objects from doc content

--- word/Editor/DocumentContent.js
+++ word/Editor/DocumentContent.js
@@ -1243,9 +1243,6 @@ CDocumentContent.prototype.Draw                           = function(nPageIndex,
         pGraphics.AddClipRect(ClipInfo.X0, Bounds.Top - Correction, Math.abs(ClipInfo.X1 - ClipInfo.X0), Bounds.Bottom - Bounds.Top + Correction);
     }
 
-    if (this.LogicDocument)
-        this.LogicDocument.DrawingObjects.drawBehindObjectsByContent(this.Get_AbsolutePage(CurPage), pGraphics, this);
-
     var Page_StartPos = this.Pages[CurPage].Pos;
     var Page_EndPos   = this.Pages[CurPage].EndPos;
     for (var Index = Page_StartPos; Index <= Page_EndPos; Index++)
