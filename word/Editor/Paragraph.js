a1c9a7fe1240ae6260f11e8781d0373f400556af
Sergey Luzyanin <Sergey.Luzyanin@onlyoffice.com>
2019-11-08 15:30:53 +0300

    [bug] Fix bug 43471

--- word/Editor/Paragraph.js
+++ word/Editor/Paragraph.js
@@ -8428,6 +8428,10 @@ Paragraph.prototype.Add_PresentationNumbering = function(_Bullet, Size, AscColor
 				else
 				{
 					this.Pr.Bullet = _OldBullet;
+					if(_OldBullet.bulletSize && !oBullet2.bulletSize)
+					{
+						oBullet2.bulletSize = _OldBullet.bulletSize.createDuplicate();
+					}
 					this.Set_Bullet(oBullet2.createDuplicate());//тип совпал, но не совпали символы. выставляем Bullet.
 																// Indent в данном случае не выставляем как это делает
 																// PowerPoint.
@@ -8443,6 +8447,10 @@ Paragraph.prototype.Add_PresentationNumbering = function(_Bullet, Size, AscColor
 		else//тип не совпал. выставляем буллет, а также проверим нужно ли выставлять Indent.
 		{
 			this.Pr.Bullet = _OldBullet;
+			if(_OldBullet.bulletSize && !oBullet2.bulletSize)
+			{
+				oBullet2.bulletSize = _OldBullet.bulletSize.createDuplicate();
+			}
 			this.Set_Bullet(oBullet2.createDuplicate());
 			LeftInd = Math.min(ParaPr.Ind.Left, ParaPr.Ind.Left + ParaPr.Ind.FirstLine);
 			var oFirstRunPr = this.Get_FirstTextPr2();
