dcc5551edef9d8029876d44271912982709d1bf1
Alexey Musinov <alexey.musinov@gmail.com>
2019-12-26 19:07:06 +0300

    [ios] fixed timer events

--- common/Native/Wrappers/common.js
+++ common/Native/Wrappers/common.js
@@ -51,40 +51,40 @@ function GetNativePageMeta(pageIndex)
 window.NativeSupportTimeouts = true;
 window.NativeTimeoutObject = {};
 
-function clearTimeout(id)
-{
-	if (!window.NativeSupportTimeouts)
-		return;
+setTimeout = window.setTimeout = function(func, interval) {
+    if (!window.NativeSupportTimeouts)
+        return;
 
-	window.NativeTimeoutObject["" + id] = undefined;
-	window["native"]["ClearTimeout"](id);
-}
-function setTimeout(func, interval)
-{
-	if (!window.NativeSupportTimeouts)
-		return;
+    var id = window["native"]["GenerateTimeoutId"](interval);
+    window.NativeTimeoutObject["" + id] = {"func": func, repeat: false};
 
-	var id = window["native"]["GenerateTimeoutId"](interval);
-	window.NativeTimeoutObject["" + id] = {"func": func, repeat: false};
-	return id;
+    return id;
 }
 
-function clearInterval(id)
-{
-	if (!window.NativeSupportTimeouts)
-		return;
+clearTimeout = window.clearTimeout = function(id) {
+    if (!window.NativeSupportTimeouts)
+        return;
 
-	window.NativeTimeoutObject["" + id] = undefined;
-	window["native"]["ClearTimeout"](id);
+    window.NativeTimeoutObject["" + id] = undefined;
+    window["native"]["ClearTimeout"](id);
 }
-function setInterval(func, interval)
-{
-	if (!window.NativeSupportTimeouts)
-		return;
 
-	var id = window["native"]["GenerateTimeoutId"](interval);
-	window.NativeTimeoutObject["" + id] = {func: func, repeat: true, interval: interval};
-	return id;
+setInterval = window.setInterval = function(func, interval) {
+    if (!window.NativeSupportTimeouts)
+        return;
+
+    var id = window["native"]["GenerateTimeoutId"](interval);
+    window.NativeTimeoutObject["" + id] = {func: func, repeat: true, interval: interval};
+
+    return id;
+}
+
+clearInterval = window.clearInterval = function(id) {
+    if (!window.NativeSupportTimeouts)
+        return;
+
+    window.NativeTimeoutObject["" + id] = undefined;
+    window["native"]["ClearTimeout"](id);
 }
 
 window.native.Call_TimeoutFire = function (id)
@@ -112,11 +112,6 @@ window.native.Call_TimeoutFire = function (id)
 
 	timeoutObject.func = null;
 };
-
-window.clearTimeout = clearTimeout;
-window.setTimeout = setTimeout;
-window.clearInterval = clearInterval;
-window.setInterval = setInterval;
 
 var console = {
 	log: function (param)
