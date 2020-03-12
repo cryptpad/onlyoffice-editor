98b7da744f3c1fa15c8e09bd2dfdbfe215c7ca15
Sergey Konovalov <Sergey.Konovalov@onlyoffice.com>
2020-03-12 11:58:30 +0300

    [se] Fix FormulaTests.html

--- common/editorscommon.js
+++ common/editorscommon.js
@@ -5139,26 +5139,28 @@
 	};
 	//------------------------------------------------------------fill polyfill--------------------------------------------
 	if (!Array.prototype.findIndex) {
-		Array.prototype.findIndex = function(predicate) {
-			if (this == null) {
-				throw new TypeError('Array.prototype.findIndex called on null or undefined');
-			}
-			if (typeof predicate !== 'function') {
-				throw new TypeError('predicate must be a function');
-			}
-			var list = Object(this);
-			var length = list.length >>> 0;
-			var thisArg = arguments[1];
-			var value;
-
-			for (var i = 0; i < length; i++) {
-				value = list[i];
-				if (predicate.call(thisArg, value, i, list)) {
-					return i;
+		Object.defineProperty(Array.prototype, 'findIndex', {
+			value: function(predicate) {
+				if (this == null) {
+					throw new TypeError('Array.prototype.findIndex called on null or undefined');
 				}
+				if (typeof predicate !== 'function') {
+					throw new TypeError('predicate must be a function');
+				}
+				var list = Object(this);
+				var length = list.length >>> 0;
+				var thisArg = arguments[1];
+				var value;
+
+				for (var i = 0; i < length; i++) {
+					value = list[i];
+					if (predicate.call(thisArg, value, i, list)) {
+						return i;
+					}
+				}
+				return -1;
 			}
-			return -1;
-		};
+		});
 	}
 	if (!Array.prototype.fill) {
 		Object.defineProperty(Array.prototype, 'fill', {
