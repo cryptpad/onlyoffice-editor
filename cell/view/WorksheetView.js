eb3b751ba59d5c5d15a0369d71c13e25b8de8cc8
Igor Zotov <Igor.Zotov@onlyoffice.com>
2020-09-22 22:35:50 +0300

    [se] Fix previous

--- cell/view/WorksheetView.js
+++ cell/view/WorksheetView.js
@@ -14899,11 +14899,9 @@
 			//в особом режиме не лочим лист при фильтрации
 			var nActive = t.model.getActiveNamedSheetView();
 			if (null !== nActive) {
+				//лочу для того, чтобы не было возможности изменить имя текущего отображения
+				//иначе будут конфликты при принятии изменений
 				api._isLockedNamedSheetView([t.model.aNamedSheetViews[nActive]], function (_success) {
-					if (!_success) {
-						t.model.workbook.handlers.trigger("asc_onError", c_oAscError.ID.LockedEditView, c_oAscError.Level.NoCritical);
-						return;
-					}
 					onChangeAutoFilterCallback(true);
 				});
 			} else {
@@ -15098,10 +15096,6 @@
 				var nActive = t.model.getActiveNamedSheetView();
 				if (null !== nActive) {
 					api._isLockedNamedSheetView([t.model.aNamedSheetViews[nActive]], function (_success) {
-						if (!_success) {
-							t.model.workbook.handlers.trigger("asc_onError", c_oAscError.ID.LockedEditView, c_oAscError.Level.NoCritical);
-							return;
-						}
 						onChangeAutoFilterCallback(true);
 					});
 				} else {
