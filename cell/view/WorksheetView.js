616d977186ade0075b6ac666c0fc872432fdd6ee
Igor Zotov <Igor.Zotov@onlyoffice.com>
2020-10-15 09:28:06 +0300

    [se] Fix bug 45935

--- cell/view/WorksheetView.js
+++ cell/view/WorksheetView.js
@@ -12456,6 +12456,10 @@
 			return res;
 		};
 
+		if (specialPasteProps.format && range.getHyperlink()) {
+			range.removeHyperlink();
+		}
+
 		//column width
 		var col = range.bbox.c1;
 		if (specialPasteProps.width && rangeStyle.colsWidth[col]) {
