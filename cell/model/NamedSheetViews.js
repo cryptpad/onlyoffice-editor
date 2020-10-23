0cd1a8bcb519aecc0ab5df84a27f82535aff6ba4
Igor Zotov <Igor.Zotov@onlyoffice.com>
2020-10-23 14:36:46 +0300

    [se] Fix bug 47050 (#1310)
    * [se] Fix bug 47050
    
    * [se] Fix read from binary

--- cell/model/NamedSheetViews.js
+++ cell/model/NamedSheetViews.js
@@ -201,7 +201,7 @@
 		}
 
 		this.name = reader.GetString2();
-		this.id = reader.GetLong();
+		this.id = reader.GetString2();
 	};
 	CT_NamedSheetView.prototype.initPostOpen = function (tableIds, ws) {
 		for (var i = 0; i < this.nsvFilters.length; ++i) {
@@ -219,7 +219,6 @@
 		}
 
 		res.name = this.name;
-		res.id = this.id;
 		res.ws = this.ws;
 
 		return res;
