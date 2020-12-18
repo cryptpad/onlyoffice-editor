d4ccd8311d216127d6eb71d9eb3c160683798011
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2020-12-18 14:08:40 +0300

    Fix

--- word/apiBuilder.js
+++ word/apiBuilder.js
@@ -11727,6 +11727,7 @@
 					case para_PresentationNumbering:
 					case para_PageNum:
 					case para_PageCount:
+					case para_End:
 					{
 						break;
 					}
@@ -11745,15 +11746,10 @@
 					}
 					case para_NewLine:
 					{
-						if (oPr && true === oPr.NewLine)
-						{
-							Str += '\r';
-							runInfo.StringCount++;
-						}
+						Str += " ";
+						runInfo.StringCount++; 
 						break;
 					}
-					case para_End:
-						break;
 				}
 			}
 			
@@ -11786,9 +11782,9 @@
 					if (oChange.pos >= oInfo.GlobStartPos || oChange.pos + DelCount >= oInfo.GlobStartPos)
 					{
 						var nPosToDel = Math.max(0, oChange.pos - oInfo.GlobStartPos + oInfo.StartPos);
-						var countToDel = Math.min(oChange.deleteCount, oInfo.StringCount - nPosToDel);
+						var countToDel = Math.min(oChange.deleteCount, oInfo.StringCount);
 
-						if (countToDel < 0)
+						if (nPosToDel >= oInfo.Run.Content.length)
 							continue;
 
 						for (var nDelChar = 0; nDelChar < countToDel; nDelChar++)
@@ -11808,7 +11804,7 @@
 								
 						}
 						
-						var nPosToAdd = oChange.pos - oInfo.GlobStartPos;
+						var nPosToAdd = Math.max(0, oChange.pos - oInfo.GlobStartPos + oInfo.StartPos);
 						for (var nAddChar = 0; nAddChar < oChange.insert.length; nAddChar++)
 						{
 							var itemText = new AscCommonWord.ParaText(oChange.insert[nAddChar]);
