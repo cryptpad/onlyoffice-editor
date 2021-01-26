f0ed767a98947d7245af5424f2e63b83852ba16e
Igor Zotov <Igor.Zotov@onlyoffice.com>
2021-01-26 12:09:09 +0300

    Feature/test formulas function (#1530)
    * [se] Add test formulas function

--- cell/apiBuilder.js
+++ cell/apiBuilder.js
@@ -524,6 +524,30 @@
 		return resultList;
 	};
 
+	Api.prototype.TestAllFormulas = function(fCallback) {
+		var formulas = this.wbModel.getAllFormulas();
+		for (var i = 0; i < formulas.length; ++i) {
+			var formula = formulas[i];
+			formula.setFormula(formula.getFormula());
+			formula.parse();
+			var formulaRes = formula.calculate();
+			var oldValue = formulaRes.getValue();
+			var cell = formula.ws.getCell3(formula.parent.nRow, formula.parent.nCol);
+			var newValue = cell.getValue();
+			if (oldValue != newValue) {
+				//error
+				fCallback({
+					sheet: formula.ws.sName,
+					r: formula.parent.nRow,
+					c: formula.parent.nCol,
+					f: formula.Formula,
+					oldValue: oldValue,
+					newValue: newValue
+				});
+			}
+		}
+	};
+
 	/**
 	 * Returns Visible of sheet
 	 * @memberof ApiWorksheet
@@ -2818,6 +2842,8 @@
 	Api.prototype["GetMailMergeData"]      = Api.prototype.GetMailMergeData;
 	
 	Api.prototype["GetRange"] = Api.prototype.GetRange;
+
+	Api.prototype["TestAllFormulas"] = Api.prototype.TestAllFormulas;
 
 	ApiWorksheet.prototype["GetVisible"] = ApiWorksheet.prototype.GetVisible;
 	ApiWorksheet.prototype["SetVisible"] = ApiWorksheet.prototype.SetVisible;
