5897e09c390a3a2bf56a51a3ce1dbe8d4b21f793
Sergey Luzyanin <Sergey.Luzyanin@onlyoffice.com>
2021-02-03 21:56:48 +0300

    [bug] Fix bug 48557

--- word/Editor/Paragraph/ComplexFieldInstruction.js
+++ word/Editor/Paragraph/ComplexFieldInstruction.js
@@ -464,7 +464,7 @@ CFieldInstructionTOC.prototype.SetPr = function(oPr)
 		this.SetSeparator(" ");
 
 	this.ForceTabLeader = oPr.TabLeader;
-	var sCaption = oPr.get_Caption();
+	var sCaption = oPr.get_CaptionForInstruction();
 	if(sCaption !== undefined)
 	{
 		if(sCaption || this.Styles.length > 0)
