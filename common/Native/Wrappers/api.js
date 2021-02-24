5a955fb6b1f5c40bc25e0c3cf419d06fe7508fa0
Alexander Yuzhin <alexander.yuzhin@onlyoffice.com>
2021-02-24 13:53:24 +0300

    [native] Handle fill form events. Allow prune js object

--- common/Native/Wrappers/api.js
+++ common/Native/Wrappers/api.js
@@ -4950,6 +4950,7 @@ Asc['asc_docs_api'].prototype.sync_EndCatchSelectedElements = function()
     }
 
     this.Send_Menu_Event(6);
+    this.sendEvent("asc_onFocusObject", this.SelectedObjectsStack);
 };
 
 function Deserialize_Table_Markup(_params, _cols, _margins, _rows)
@@ -5911,6 +5912,8 @@ function NativeOpenFile3(_params, documentInfo)
                                   window["native"]["OnCallMenuEvent"](2404, stream); // ASC_SPREADSHEETS_EVENT_TYPE_COLOR_SCHEMES
                                   });
 
+        _api.asc_registerCallback("asc_onFocusObject", onFocusObject);
+        
         // Comments
 
         _api.asc_registerCallback("asc_onAddComment", onApiAddComment);
@@ -5929,6 +5932,11 @@ function NativeOpenFile3(_params, documentInfo)
 
         _api.asc_registerCallback("asc_onDocumentPlaceChanged", onDocumentPlaceChanged);
         _api.asc_registerCallback("asc_onShowRevisionsChange", onApiShowRevisionsChange);
+
+        // Fill forms
+        
+        _api.asc_registerCallback('asc_onShowContentControlsActions', onShowContentControlsActions);
+        _api.asc_registerCallback('asc_onHideContentControlsActions', onHideContentControlsActions);
 
         // Co-authoring
 
@@ -6406,6 +6414,149 @@ function onApiShowRevisionsChange(data) {
     postDataAsJSONString(revisionChanges, 24001); // ASC_MENU_EVENT_TYPE_SHOW_REVISIONS_CHANGE
 };
 
+// Fill forms
+
+function onShowContentControlsActions(obj, x, y) {
+    console.log("onShowContentControlsActions x: " + x + " y: " + y);
+    
+    var type = obj.type;
+    var data = {
+        "x": x,
+        "y": y,
+        "type": type
+    };
+    var contentControllJSON = {};
+
+    switch (type) {
+        case Asc.c_oAscContentControlSpecificType.DateTime:
+            contentControllJSON = contentControllDateTimeToJSON(obj);
+            break;
+        case Asc.c_oAscContentControlSpecificType.Picture:
+            if (obj.pr && obj.pr.get_Lock) {
+                var lock = obj.pr.get_Lock();
+                if (lock == Asc.c_oAscSdtLockType.SdtContentLocked || lock == Asc.c_oAscSdtLockType.ContentLocked)
+                    return;
+            }
+            break;
+        case Asc.c_oAscContentControlSpecificType.DropDownList:
+        case Asc.c_oAscContentControlSpecificType.ComboBox:
+            contentControllJSON = contentControllListAToJSON(obj);
+            break;
+    }
+
+    // merge
+    for(var key in contentControllJSON) {
+        data[key] = contentControllJSON[key];
+    }
+
+    postDataAsJSONString(data, 26001); // ASC_MENU_EVENT_TYPE_SHOW_CONTENT_CONTROLS_ACTIONS
+}
+
+function onHideContentControlsActions() {
+    console.log("onHideContentControlsActions");
+    postDataAsJSONString(null, 26002); // ASC_MENU_EVENT_TYPE_HIDE_CONTENT_CONTROLS_ACTIONS
+}
+
+function contentControllDateTimeToJSON(obj) {
+    var props = obj.pr,
+        specProps = props.get_DateTimePr();
+
+    return {
+        date: specProps ? specProps.get_FullDate() : null
+    }
+}
+
+function contentControllListAToJSON(obj) {
+    var type = obj.type,
+        props = obj.pr,
+        specProps = (type == Asc.c_oAscContentControlSpecificType.ComboBox) ? props.get_ComboBoxPr() : props.get_DropDownListPr(),
+        isForm = !!props.get_FormPr(),
+        internalId = props.get_InternalId()
+        items = [];
+
+    if (specProps) {
+        if (isForm) { // for dropdown and combobox form control always add placeholder item
+            var text = props.get_PlaceholderText();
+            items.push({
+                caption: text,
+                value: ''
+            });
+        }
+        var count = specProps.get_ItemsCount();
+        for (var i = 0; i < count; i++) {
+            (specProps.get_ItemValue(i) !== '' || !isForm) && items.push({
+                caption: specProps.get_ItemDisplayText(i),
+                value: specProps.get_ItemValue(i)
+            });
+        }
+        if (!isForm && menu.items.length < 1) {
+            items.push({
+                caption: '',
+                value: '-1'
+            });
+        }
+    }
+
+    return {
+        internalId: internalId,
+        isForm: isForm,
+        items: items
+    }
+}
+
+// Common
+
+function onFocusObject(SelectedObjects) {
+    console.log("call onFocusObject");
+
+    var settings = [];
+    var isChart = false;
+    var control_props = _api.asc_IsContentControl() ? _api.asc_GetContentControlProperties() : null,
+        control_lock = false;
+
+    for (i = 0; i < SelectedObjects.length; i++) {
+        var content_locked = false;
+        var eltype = SelectedObjects[i].get_ObjectType();
+        var value = SelectedObjects[i].get_ObjectValue();
+        
+        switch (eltype)
+        {
+            case Asc.c_oAscTypeSelectElement.Paragraph:
+            case Asc.c_oAscTypeSelectElement.Header:
+            case Asc.c_oAscTypeSelectElement.Table:
+            case Asc.c_oAscTypeSelectElement.Image:
+            case Asc.c_oAscTypeSelectElement.Hyperlink:
+            case Asc.c_oAscTypeSelectElement.Math:
+            {
+                settings.push({
+                    type: eltype,
+                    value: JSON.prune(value, 5)
+                });
+                break;
+            }
+            case Asc.c_oAscTypeSelectElement.SpellCheck:
+            default:
+            {
+                break;
+            }
+        }
+    }
+
+    // Form object
+    if (control_props && control_props.get_FormPr()) {
+        var spectype = control_props.get_SpecificType();
+        settings.push({
+            type: Asc.c_oAscTypeSelectElement.ContentControl,
+            spectype: spectype,
+            value: JSON.prune(control_props, 4)
+        });
+    }
+
+    postDataAsJSONString(settings, 26101); // ASC_MENU_EVENT_TYPE_FOCUS_OBJECT
+}
+
+// Others
+
 var DocumentPageSize = new function()
 {
     this.oSizes    = [{name : "US Letter", w_mm : 215.9, h_mm : 279.4, w_tw : 12240, h_tw : 15840},
@@ -6650,3 +6801,171 @@ window["AscCommon"].sendImgUrls = function(api, images, callback)
 };
 
 window["native"]["offline_of"] = function(_params, documentInfo) {NativeOpenFile3(_params, documentInfo);};
+
+
+
+
+// JSON.prune : a function to stringify any object without overflow
+// two additional optional parameters :
+//   - the maximal depth (default : 6)
+//   - the maximal length of arrays (default : 50)
+// You can also pass an "options" object.
+// examples :
+//   var json = JSON.prune(window)
+//   var arr = Array.apply(0,Array(1000)); var json = JSON.prune(arr, 4, 20)
+//   var json = JSON.prune(window.location, {inheritedProperties:true})
+// Web site : http://dystroy.org/JSON.prune/
+// JSON.prune on github : https://github.com/Canop/JSON.prune
+// This was discussed here : http://stackoverflow.com/q/13861254/263525
+// The code is based on Douglas Crockford's code : https://github.com/douglascrockford/JSON-js/blob/master/json2.js
+// No effort was done to support old browsers. JSON.prune will fail on IE8.
+(function () {
+	'use strict';
+
+	var DEFAULT_MAX_DEPTH = 6;
+	var DEFAULT_ARRAY_MAX_LENGTH = 50;
+	var DEFAULT_PRUNED_VALUE = '"-pruned-"';
+	var seen; // Same variable used for all stringifications
+	var iterator; // either forEachEnumerableOwnProperty, forEachEnumerableProperty or forEachProperty
+
+	// iterates on enumerable own properties (default behavior)
+	var forEachEnumerableOwnProperty = function(obj, callback) {
+		for (var k in obj) {
+			if (Object.prototype.hasOwnProperty.call(obj, k)) callback(k);
+		}
+	};
+	// iterates on enumerable properties
+	var forEachEnumerableProperty = function(obj, callback) {
+		for (var k in obj) callback(k);
+	};
+	// iterates on properties, even non enumerable and inherited ones
+	// This is dangerous
+	var forEachProperty = function(obj, callback, excluded) {
+		if (obj==null) return;
+		excluded = excluded || {};
+		Object.getOwnPropertyNames(obj).forEach(function(k){
+			if (!excluded[k]) {
+				callback(k);
+				excluded[k] = true;
+			}
+		});
+		forEachProperty(Object.getPrototypeOf(obj), callback, excluded);
+	};
+
+	Object.defineProperty(Date.prototype, "toPrunedJSON", {value:Date.prototype.toJSON});
+
+	var	cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
+		escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
+		meta = {	// table of character substitutions
+			'\b': '\\b',
+			'\t': '\\t',
+			'\n': '\\n',
+			'\f': '\\f',
+			'\r': '\\r',
+			'"' : '\\"',
+			'\\': '\\\\'
+		};
+
+	function quote(string) {
+		escapable.lastIndex = 0;
+		return escapable.test(string) ? '"' + string.replace(escapable, function (a) {
+			var c = meta[a];
+			return typeof c === 'string'
+				? c
+				: '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
+		}) + '"' : '"' + string + '"';
+	}
+
+
+	var prune = function (value, depthDecr, arrayMaxLength) {
+		var prunedString = DEFAULT_PRUNED_VALUE;
+		var replacer;
+		if (typeof depthDecr == "object") {
+			var options = depthDecr;
+			depthDecr = options.depthDecr;
+			arrayMaxLength = options.arrayMaxLength;
+			iterator = options.iterator || forEachEnumerableOwnProperty;
+			if (options.allProperties) iterator = forEachProperty;
+			else if (options.inheritedProperties) iterator = forEachEnumerableProperty
+			if ("prunedString" in options) {
+				prunedString = options.prunedString;
+			}
+			if (options.replacer) {
+				replacer = options.replacer;
+			}
+		} else {
+			iterator = forEachEnumerableOwnProperty;
+		}
+		seen = [];
+		depthDecr = depthDecr || DEFAULT_MAX_DEPTH;
+		arrayMaxLength = arrayMaxLength || DEFAULT_ARRAY_MAX_LENGTH;
+		function str(key, holder, depthDecr) {
+			var i, k, v, length, partial, value = holder[key];
+
+			if (value && typeof value === 'object' && typeof value.toPrunedJSON === 'function') {
+				value = value.toPrunedJSON(key);
+			}
+			if (value && typeof value.toJSON === 'function') {
+				value = value.toJSON();
+			}
+
+			switch (typeof value) {
+			case 'string':
+				return quote(value);
+			case 'number':
+				return isFinite(value) ? String(value) : 'null';
+			case 'boolean':
+			case 'null':
+				return String(value);
+			case 'object':
+				if (!value) {
+					return 'null';
+				}
+				if (depthDecr<=0 || seen.indexOf(value)!==-1) {
+					if (replacer) {
+						var replacement = replacer(value, prunedString, true);
+						return replacement===undefined ? undefined : ''+replacement;
+					}
+					return prunedString;
+				}
+				seen.push(value);
+				partial = [];
+				if (Object.prototype.toString.apply(value) === '[object Array]') {
+					length = Math.min(value.length, arrayMaxLength);
+					for (i = 0; i < length; i += 1) {
+						partial[i] = str(i, value, depthDecr-1) || 'null';
+					}
+					v = '[' + partial.join(',') + ']';
+					if (replacer && value.length>arrayMaxLength) return replacer(value, v, false);
+					return v;
+				}
+				if (value instanceof RegExp) {
+					return quote(value.toString());
+				}
+				iterator(value, function(k) {
+					try {
+						v = str(k, value, depthDecr-1);
+						if (v) partial.push(quote(k) + ':' + v);
+					} catch (e) {
+						// this try/catch due to forbidden accessors on some objects
+					}
+				});
+				return '{' + partial.join(',') + '}';
+			case 'function':
+			case 'undefined':
+				return replacer ? replacer(value, undefined, false) : undefined;
+			}
+		}
+		return str('', {'': value}, depthDecr);
+	};
+
+	prune.log = function() {
+		console.log.apply(console, Array.prototype.map.call(arguments, function(v) {
+			return JSON.parse(JSON.prune(v));
+		}));
+	};
+	prune.forEachProperty = forEachProperty; // you might want to also assign it to Object.forEachProperty
+
+	if (typeof module !== "undefined") module.exports = prune;
+	else JSON.prune = prune;
+}());
