f224ae4ce7a193075949e0449fc3fe5f37bb7b94
GoshaZotov <Igor.Zotov@onlyoffice.com>
2021-03-25 10:31:13 +0300

    [se] Fix typo

--- cell/view/WorksheetView.js
+++ cell/view/WorksheetView.js
@@ -11401,7 +11401,7 @@
 		}
 
 		//conditional formatting
-		if (specialPasteProps.val && specialPasteProps.format) {
+		if (specialPasteProps.val && specialPasteProps.format && fromBinary) {
 			var offsetAll = new AscCommon.CellBase(arnToRange.r1 - refInsertBinary.r1, arnToRange.c1 - refInsertBinary.c1);
 			t.model.moveConditionalFormatting(refInsertBinary, arnToRange, true, offsetAll, this.model, val);
 		}
