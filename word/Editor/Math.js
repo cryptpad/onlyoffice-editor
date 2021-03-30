19518816764d3a2d6b2a31173cb22c0393ab629e
Kulikova Svetlana <Svetlana.Kulikova@onlyoffice.com>
2021-03-30 13:01:39 +0300

    Fix bug 49338 (#1735)

--- word/Editor/Math.js
+++ word/Editor/Math.js
@@ -2669,7 +2669,7 @@ ParaMath.prototype.MathToImageConverter = function(bCopy, _canvasInput, _widthPx
         _canvas = new CNativeGraphics();
         _canvas.width  = _width;
         _canvas.height = _height;
-        _canvas.init(window["native"], _width, _height, _width / dKoef, _height / dKoef);
+        _canvas.create(window["native"], _width, _height, _width / dKoef, _height / dKoef);
         _canvas.CoordTransformOffset(_widthPx  !== undefined ? (_widthPx  - w_px) / 2 : 0,
                                      _heightPx !== undefined ? (_heightPx - h_px) / 2 : 0);
         _canvas.transform(1, 0, 0, 1, 0, 0);
