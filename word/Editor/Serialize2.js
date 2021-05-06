8745402f430a8cbad9eaff7253ebca3c2019b4a0
Sergey Konovalov <Sergey.Konovalov@onlyoffice.com>
2021-05-06 11:23:55 +0300

    [de] Fix bug with 1970 comment date after copy-paste

--- word/Editor/Serialize2.js
+++ word/Editor/Serialize2.js
@@ -6740,7 +6740,7 @@ function BinaryCommentsTableWriter(memory, doc, oMapCommentId, commentUniqueGuid
 		{
 			this.bs.WriteItem(c_oSer_CommentsType.Replies, function(){oThis.WriteReplies(comment.m_aReplies);});
 		}
-		if (null != comment.m_sOOTime)
+		if (null != comment.m_sOOTime && "" != comment.m_sOOTime)
 		{
 			this.memory.WriteByte(c_oSer_CommentsType.DateUtc);
 			this.memory.WriteString2(new Date(comment.m_sOOTime - 0).toISOString().slice(0, 19) + 'Z');
@@ -8303,6 +8303,8 @@ function BinaryFileReader(doc, openParams)
 				oCommentObj.m_sProviderId = comment.ProviderId;
 			if(null != comment.Date)
 				oCommentObj.m_sTime = comment.Date;
+			if(null != comment.OODate)
+				oCommentObj.m_sOOTime = comment.OODate;
 			if(null != comment.m_sQuoteText)
 				oCommentObj.m_sQuoteText = comment.m_sQuoteText;
 			if(null != comment.Text)
