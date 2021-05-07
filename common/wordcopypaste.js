854939327cfd64388dc032cf75e27856f2671fa2
Igor Zotov <Igor.Zotov@onlyoffice.com>
2021-05-07 11:52:56 +0300

    Fix/bug 50004 (#1926)
    * [se] For bug 50004

--- common/wordcopypaste.js
+++ common/wordcopypaste.js
@@ -538,33 +538,43 @@ CopyProcessor.prototype =
             this.ParseItem(Item.Content[i], oTarget, Item.Content[i + 1], Item.Content.length);
     },
     CopyRunContent: function (Container, oTarget, bOmitHyperlink) {
-        for (var i = 0; i < Container.Content.length; i++) {
-            var item = Container.Content[i];
-            if (para_Run === item.Type) {
+		var bookmarksStartMap = {};
+		var bookmarkPrviousTargetMap = {};
+		var bookmarkLevel = 0;
+
+		var closeBookmarks = function (_level) {
+			var tempTarget = bookmarkPrviousTargetMap[_level];
+			if (tempTarget) {
+				tempTarget.addChild(oTarget);
+				oTarget = tempTarget;
+			}
+		};
+
+    	for (var i = 0; i < Container.Content.length; i++) {
+			var item = Container.Content[i];
+			if (para_Run === item.Type) {
 				var oSpan = new CopyElement("span");
-                this.CopyRun(item, oSpan);
-                if(!oSpan.isEmptyChild()){
+				this.CopyRun(item, oSpan);
+				if (!oSpan.isEmptyChild()) {
 					this.parse_para_TextPr(item.Get_CompiledTextPr(), oSpan);
 					oTarget.addChild(oSpan);
 				}
-            }
-            else if (para_Hyperlink === item.Type) {
-                if (!bOmitHyperlink) {
+			} else if (para_Hyperlink === item.Type) {
+				if (!bOmitHyperlink) {
 					var oHyperlink = new CopyElement("a");
-                    var sValue = item.GetValue();
-                    var sToolTip = item.GetToolTip();
+					var sValue = item.IsAnchor() ? "#" + item.Anchor : item.GetValue();
+					var sToolTip = item.GetToolTip();
 					oHyperlink.oAttributes["href"] = CopyPasteCorrectString(sValue);
 					oHyperlink.oAttributes["title"] = CopyPasteCorrectString(sToolTip);
 					//вложенные ссылки в html запрещены.
 					this.CopyRunContent(item, oHyperlink, true);
 					oTarget.addChild(oHyperlink);
-                }
-				else
+				} else {
 					this.CopyRunContent(item, oTarget, true);
-            }
-			else if(para_Math === item.Type){
-                var sSrc = item.MathToImageConverter();
-				if (null != sSrc && null != sSrc.ImageUrl){
+				}
+			} else if (para_Math === item.Type) {
+				var sSrc = item.MathToImageConverter();
+				if (null != sSrc && null != sSrc.ImageUrl) {
 					var oImg = new CopyElement("img");
 					if (sSrc.w_px > 0) {
 						oImg.oAttributes["width"] = sSrc.w_px;
@@ -575,11 +585,39 @@ CopyProcessor.prototype =
 					oImg.oAttributes["src"] = sSrc.ImageUrl;
 					oTarget.addChild(oImg);
 				}
-			} else if(para_InlineLevelSdt === item.Type) {
+			} else if (para_InlineLevelSdt === item.Type) {
 				this.CopyRunContent(item, oTarget);
-			} else if(para_Field === item.Type)
+			} else if (para_Field === item.Type) {
 				this.CopyRunContent(item, oTarget);
-        }
+			} else if (para_Bookmark === item.Type) {
+				//для внутренних ссылок
+				//если конец ссылки находится в тепкущем параграфе, то закрываем тэг ссылки здесь
+				//если он находится в следующем параграфе, то закрываем после того, как прошлись по всему содержимому данного параграфа
+				//ms в данном случае берёт только первый элемент
+				//чтобы заранее не проходиться по всему контенту параграфа в поисках закрытия bookmark - закрываю его после всего цикла
+				//на следующий параграф не переносим
+				if (item.Start) {
+					bookmarkLevel++;
+					bookmarksStartMap[item.BookmarkId] = 1;
+					var oBookmark = new CopyElement("a");
+					var name = item.GetBookmarkName();
+					oBookmark.oAttributes["name"] = CopyPasteCorrectString(name);
+					bookmarkPrviousTargetMap[bookmarkLevel] = oTarget;
+					oTarget = oBookmark;
+				} else if (bookmarksStartMap[item.BookmarkId]) {
+					bookmarksStartMap[item.BookmarkId] = 0;
+					closeBookmarks(bookmarkLevel);
+					bookmarkLevel--;
+				}
+			}
+		}
+
+		if (bookmarkLevel > 0) {
+    		while(bookmarkLevel > 0) {
+				closeBookmarks(bookmarkLevel);
+				bookmarkLevel--;
+			}
+		}
     },
     CopyParagraph : function(oDomTarget, Item, selectedAll, nextElem)
     {
