c50c2dceaef1baf0d5b88e773aac17eb966a1a89
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2021-05-28 14:08:55 +0300

    Fixed pasting text into cells of Api.ReplaceTextSmart() method in SpreadSheet editor

--- word/apiBuilder.js
+++ word/apiBuilder.js
@@ -12035,7 +12035,10 @@
 					nTextToReplace += nCountLinesInCell;
 
 					if (resultText !== '')
-						tempRange.SetValue(resultText);
+						if (!this.wb.getCellEditMode())
+							tempRange.SetValue(resultText);
+						else
+							this.wb.cellEditor._addChars(resultText);
 				}
 			}
 		}
