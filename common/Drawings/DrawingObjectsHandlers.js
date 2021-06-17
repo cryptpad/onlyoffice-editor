300178076c56d747c4798bd9a8a668f3d57f9e2f
Sergey Luzyanin <Sergey.Luzyanin@onlyoffice.com>
2021-06-17 20:02:10 +0300

    [forms] Select form shape on click on internal image

--- common/Drawings/DrawingObjectsHandlers.js
+++ common/Drawings/DrawingObjectsHandlers.js
@@ -456,6 +456,11 @@ function handleShapeImage(drawing, drawingObjectsController, e, x, y, group, pag
     {
         if(bWord/* && (!drawing.txWarpStruct || drawingObjectsController.curState.startTargetTextObject === drawing || drawing.haveSelectedDrawingInContent && drawing.haveSelectedDrawingInContent())*/)
         {
+            if(drawing.getObjectType() === AscDFH.historyitem_type_Shape &&
+                drawing.isForm() && drawing.getInnerForm() && drawing.getInnerForm().IsPicture())
+            {
+                return drawingObjectsController.handleMoveHit(drawing, e, x, y, group, false, pageIndex, bWord);
+            }
             var all_drawings = drawing.getDocContent().GetAllDrawingObjects();
             var drawings2 = [];
             for(var i = 0; i < all_drawings.length; ++i)
