bc0c241f1397ce1d06cc062b8258e6bdcb4d0dc0
Sergey Konovalov <Sergey.Konovalov@onlyoffice.com>
2021-07-19 15:40:31 +0300

    [all] Fix theme reading

--- word/Editor/Serialize2.js
+++ word/Editor/Serialize2.js
@@ -15972,7 +15972,6 @@ function Binary_OtherTableReader(doc, oReadResult, stream)
 		else if ( c_oSerOtherTableTypes.DocxTheme === type )
         {
 		    this.Document.theme = pptx_content_loader.ReadTheme(this, this.stream);
-		    res = c_oSerConstants.ReadUnknown;
 		}
         else
             res = c_oSerConstants.ReadUnknown;
