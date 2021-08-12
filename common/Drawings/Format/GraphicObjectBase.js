63b21fc7865037b34ed0959c2a3a85c3c9f271fb
Vladimir Privezenov <privezenoff@yandex.ru>
2021-08-12 23:55:49 +0300

    fix: fix coefficients

--- common/Drawings/Format/GraphicObjectBase.js
+++ common/Drawings/Format/GraphicObjectBase.js
@@ -2385,22 +2385,22 @@
                 var defaultExtY = this.extY;
                 if (prSet) {
                     if (prSet.custScaleX) {
-                        defaultExtX /= prSet.custScaleX / 100000;
+                        defaultExtX /= prSet.custScaleX;
                     }
                     if (prSet.custScaleY) {
-                        defaultExtY /= prSet.custScaleY / 100000;
+                        defaultExtY /= prSet.custScaleY;
                     }
                     if (prSet.custLinFactNeighborX) {
-                        originalPosX -= (prSet.custLinFactNeighborX / 100000) * defaultExtX;
+                        originalPosX -= (prSet.custLinFactNeighborX) * defaultExtX;
                     }
                     if (prSet.custLinFactNeighborY) {
-                        originalPosY -= (prSet.custLinFactNeighborY / 100000) * defaultExtY;
+                        originalPosY -= (prSet.custLinFactNeighborY) * defaultExtY;
                     }
                     if (posX !== this.x) {
-                        prSet.setCustLinFactNeighborX(((posX - originalPosX) / defaultExtX) * 100000);
+                        prSet.setCustLinFactNeighborX(((posX - originalPosX) / defaultExtX));
                     }
                     if (posY !== this.y) {
-                        prSet.setCustLinFactNeighborY(((posY - originalPosY) / defaultExtY) * 100000);
+                        prSet.setCustLinFactNeighborY(((posY - originalPosY) / defaultExtY));
                     }
                 }
             }
