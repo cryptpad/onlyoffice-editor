5f68cd7ebb691e71d0ab75f0a0f3bebe443c7da3
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2021-08-30 19:29:10 +0300

    Feature/for plugins v1 (#2198)
    * Add params: events and isSystem in function CPluginVariation.prototype["deserialize"] and param minVersion in CPlugin.prototype["deserialize"]
    
    * Fixed parsing events parameter
    
    Co-authored-by: AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>

--- common/apiCommon.js
+++ common/apiCommon.js
@@ -5473,6 +5473,7 @@
 
 		this.isVisual     = (_object["isVisual"] != null) ? _object["isVisual"] : this.isVisual;
 		this.isModal      = (_object["isModal"] != null) ? _object["isModal"] : this.isModal;
+		this.isSystem     = (_object["isSystem"] != null) ? _object["isSystem"] : this.isSystem;
 		this.isInsideMode = (_object["isInsideMode"] != null) ? _object["isInsideMode"] : this.isInsideMode;
 		this.isCustomWindow = (_object["isCustomWindow"] != null) ? _object["isCustomWindow"] : this.isCustomWindow;
 
@@ -5482,6 +5483,8 @@
 		this.isUpdateOleOnResize = (_object["isUpdateOleOnResize"] != null) ? _object["isUpdateOleOnResize"] : this.isUpdateOleOnResize;
 
 		this.buttons = (_object["buttons"] != null) ? _object["buttons"] : this.buttons;
+
+		if (_object["events"] != null) this.set_Events(_object["events"]);
 
 		this.size = (_object["size"] != null) ? _object["size"] : this.size;
 		this.initOnSelectionChanged = (_object["initOnSelectionChanged"] != null) ? _object["initOnSelectionChanged"] : this.initOnSelectionChanged;
@@ -5557,6 +5560,7 @@
 		this.name       = (_object["name"] != null) ? _object["name"] : this.name;
 		this.guid       = (_object["guid"] != null) ? _object["guid"] : this.guid;
 		this.baseUrl    = (_object["baseUrl"] != null) ? _object["baseUrl"] : this.baseUrl;
+		this.minVersion = (_object["minVersion"] != null) ? _object["minVersion"] : this.minVersion;
 		this.variations = [];
 		for (var i = 0; i < _object["variations"].length; i++)
 		{
