5e7b58c39de1c9f8c2ee7731c76ee13e82a6584a
GoshaZotov <Igor.Zotov@onlyoffice.com>
2021-09-29 17:00:34 +0300

    [se] Fix bug 52837

--- cell/model/Workbook.js
+++ cell/model/Workbook.js
@@ -7254,6 +7254,10 @@
 				wsFrom = this;
 			}
 
+			//TODO проверить, необходимо ли чистить?
+			//чистим ту область, куда переносим
+			//wsTo.removeSparklineGroup
+
 			wsFrom.aSparklineGroups.forEach(function (val) {
 				if (val) {
 					var aSparklines = [];
@@ -7285,7 +7289,7 @@
 						}
 
 						//необходимо ещё сдвинуть _f
-						if (_elem && _elem._f && oBBoxFrom.containsRange(_elem._f) && cloneElem._f) {
+						if (_elem && _elem._f && cloneElem._f) {
 							cloneElem._f.setOffset(offset);
 							if (wsTo.sName !== cloneElem._f.sheet) {
 								cloneElem._f.setSheet(wsTo.sName);
