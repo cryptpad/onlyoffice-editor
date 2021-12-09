17b73839c03b5ebdc71e49ce679f35df940a5922
KirillovIlya <ilya.kirillov@onlyoffice.com>
2021-12-09 15:03:57 +0300

    Fix bug #54206
    Fix the problem with non-breaking space

--- common/editorscommon.js
+++ common/editorscommon.js
@@ -4297,10 +4297,10 @@
 		return isCeil ? Math.ceil(nFontSize * 2) / 2 : Math.floor(nFontSize * 2) / 2;
 	}
 
+	// Non-breaking-space сюда не добавлять!
 	var c_oAscSpaces = [];
 	c_oAscSpaces[0x000A] = 1;
 	c_oAscSpaces[0x0020] = 1;
-	c_oAscSpaces[0x00A0] = 1;
 	c_oAscSpaces[0x2002] = 1;
 	c_oAscSpaces[0x2003] = 1;
 	c_oAscSpaces[0x2005] = 1;
@@ -4308,7 +4308,7 @@
 	c_oAscSpaces[0xFEFF] = 1;
 
 	/**
-	 * Проверяем является ли заданный юников пробелом
+	 * Проверяем является ли заданный юникод пробелом
 	 * @param nUnicode {number}
 	 * @returns {boolean}
 	 */
