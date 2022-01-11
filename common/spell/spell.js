adfc40c52eb931181b5d7931891b7261dba358ac
yflory <yann.flory@xwiki.com>
2022-01-11 15:17:53 +0100

    Disable spellcheck worker

--- common/spell/spell.js
+++ common/spell/spell.js
@@ -138,6 +138,7 @@ function CSpellchecker(settings)
 
 	this.restart = function()
 	{
+        return; // XXX CRYPTPAD HACK
 		this.stop();
 
 		var worker_src = this.useWasm ? "spell.js" : "spell_ie.js";
