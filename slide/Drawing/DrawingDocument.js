26f41f97934e3b3aee41e0016b79b84c85bba62c
Vladimir Privezenov <privezenoff@yandex.ru>
2022-01-13 15:52:59 +0300

    Fix bug #54865

--- slide/Drawing/DrawingDocument.js
+++ slide/Drawing/DrawingDocument.js
@@ -3478,7 +3478,7 @@ function CDrawingDocument()
                     var correctNum = 1;
                     if (levelNum === level.Text[i].Value)
                         correctNum = counterCurrent;
-                    text += AscCommon.IntToNumberFormat(correctNum, level.Format, level.GetOLang());
+                    text += AscCommon.IntToNumberFormat(correctNum, level.Format, level.get_OLang());
                     break;
                 default:
                     break;
@@ -3703,7 +3703,7 @@ function CDrawingDocument()
 							text += curLvl.Text[j].Value;
 							break;
 						case Asc.c_oAscNumberingLvlTextType.Num:
-							text += AscCommon.IntToNumberFormat(1, curLvl.Format, curLvl.GetOLang());
+							text += AscCommon.IntToNumberFormat(1, curLvl.Format, curLvl.get_OLang());
 							break;
 						default:
 							break;
@@ -3840,7 +3840,7 @@ function CDrawingDocument()
 							var correctNum = 1;
 							if (levelNum === props[i].Text[k].Value)
 								correctNum = counterCurrent;
-							text += AscCommon.IntToNumberFormat(correctNum, props[i].Format, props[i].GetOLang());
+							text += AscCommon.IntToNumberFormat(correctNum, props[i].Format, props[i].get_OLang());
 							break;
 						default:
 							break;
