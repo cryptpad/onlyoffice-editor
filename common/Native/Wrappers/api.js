6f0afc069b72c0ac1ae3ab587928bd128fc50433
Alexander Yuzhin <alexander.yuzhin@onlyoffice.com>
2022-02-15 11:35:41 +0300

    Fix bug #55460

--- common/Native/Wrappers/api.js
+++ common/Native/Wrappers/api.js
@@ -2779,6 +2779,32 @@ Asc['asc_docs_api'].prototype["Call_Menu_Event"] = function(type, _params)
             break;
         }
 
+        case 26004: // ASC_MENU_EVENT_TYPE_DO_SET_CONTENTCONTROL_PICTURE_URL
+        {
+            var urls = JSON.parse(_params[0]);
+            AscCommon.g_oDocumentUrls.addUrls(urls);
+            var firstUrl;
+            for (var i in urls) {
+                if (urls.hasOwnProperty(i)) {
+                    firstUrl = urls[i];
+                    break;
+                }
+            }
+
+            var _src = firstUrl;
+            var _w = _params[1];
+            var _h = _params[2];
+            var _pageNum = _params[3];
+            var _additionalParams = _params[4];
+
+            var json = JSON.parse(_additionalParams);
+            if (json) {
+                var internalId = json["internalId"] || "";
+                _api.SetContentControlPictureUrlNative(_src, internalId)
+            }
+            break;
+        }
+
         default:
             break;
     }
