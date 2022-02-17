b66cea69f120dacc273ae65f0feb81b3146485ab
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2022-02-17 15:22:05 +0300

    Fix bug #53545

--- word/apiBuilder.js
+++ word/apiBuilder.js
@@ -4543,6 +4543,15 @@
 			return null;
 
 		var oSectPr = new CSectionPr(this.Document);
+
+		var nContentPos = this.Document.CurPos.ContentPos;
+		var oCurSectPr  = this.Document.SectionsInfo.Get_SectPr(nContentPos).SectPr;
+
+		oSectPr.Copy(oCurSectPr);
+		oCurSectPr.Set_Type(oSectPr.Type);
+		oCurSectPr.Set_PageNum_Start(-1);
+		oCurSectPr.Clear_AllHdrFtr();
+
 		oParagraph.private_GetImpl().Set_SectionPr(oSectPr);
 		return new ApiSection(oSectPr);
 	};
