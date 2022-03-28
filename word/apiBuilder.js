52caf3cbee5e1c1f3cd5222a54d4f4a4576cadd4
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2022-03-28 13:05:55 +0300

    Refactoring ApiDocument.prototype.CreateSection method

--- word/apiBuilder.js
+++ word/apiBuilder.js
@@ -4642,12 +4642,15 @@
 	 * @memberof ApiDocument
 	 * @typeofeditors ["CDE"]
 	 * @param {ApiParagraph} oParagraph - The paragraph after which a new document section will be inserted.
+	 * Paragraph must be in a document.
 	 * @returns {ApiSection}
 	 */
 	ApiDocument.prototype.CreateSection = function(oParagraph)
 	{
-		if (!(oParagraph instanceof ApiParagraph) || !oParagraph.Paragraph.CanAddSectionPr())
-			return null;
+		if (!(oParagraph instanceof ApiParagraph))
+			return new Error('Parameter is invalid.');
+		if (!oParagraph.Paragraph.CanAddSectionPr())
+			return new Error('Paragraph must be in a document.');
 
 		var oSectPr = new CSectionPr(this.Document);
 
