975b2465c695e46f972a5efa5aa81b950471bb92
Igor Zotov <Igor.Zotov@onlyoffice.com>
2022-08-04 10:52:05 +0300

    [se] Fix bug 58381

--- cell/utils/utils.js
+++ cell/utils/utils.js
@@ -570,6 +570,17 @@
 			return bRes;
 		};
 
+		Range.prototype.isIntersectWithRanges = function (ranges, exceptionIndex) {
+			if (ranges) {
+				for (var i = 0; i < ranges.length; i++) {
+					if ((null == exceptionIndex || (null != exceptionIndex && i !== exceptionIndex)) && this.isIntersect(ranges[i])) {
+						return true;
+					}
+				}
+			}
+			return false;
+		};
+
 		Range.prototype.isIntersectForShift = function(range, offset) {
 			var isHor = offset && offset.col;
 			var isDelete = offset && (offset.col < 0 || offset.row < 0);
