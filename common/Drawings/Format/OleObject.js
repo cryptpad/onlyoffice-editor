0818a7f5042e44acac97a8b4909ac5bdb79db50e
Vladimir Privezenov <privezenoff@yandex.ru>
2022-09-15 14:29:06 +0300

    Fix open images in frame editor

--- common/Drawings/Format/OleObject.js
+++ common/Drawings/Format/OleObject.js
@@ -610,12 +610,14 @@ function (window, undefined) {
             const sData = AscCommon.Base64.encode(oOleObject.m_aBinaryData);
             const nImageWidth = oOleObject.extX * AscCommon.g_dKoef_mm_to_pix;
             const nImageHeight = oOleObject.extY * AscCommon.g_dKoef_mm_to_pix;
+            const documentImageUrls = AscCommon.g_oDocumentUrls.urls;
 
             return {
                 "binary": "XLSY;v2;" + nDataSize  + ";" + sData,
                 "isFromSheetEditor": !!oOleObject.worksheet,
                 "imageWidth": nImageWidth,
-                "imageHeight": nImageHeight
+                "imageHeight": nImageHeight,
+                "documentImageUrls": documentImageUrls
             };
         }
         return {
