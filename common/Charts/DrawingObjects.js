8e989f3ad9cd5c0fda86c8ebad150d6235e235fb
Vladimir Privezenov <privezenoff@yandex.ru>
2022-09-20 17:56:28 +0300

    Revert "Fix draw single chart for ole object preview"
    This reverts commit 2c45a086381ed905bc1af073e6521da3df0e46da.

--- common/Charts/DrawingObjects.js
+++ common/Charts/DrawingObjects.js
@@ -1385,6 +1385,16 @@ CSparklineView.prototype.setMinMaxValAx = function(minVal, maxVal, oSparklineGro
         return false;
     };
 
+    DrawingBase.prototype.createImage = function() {
+        var koef = Asc.getCvtRatio(3, 0, this.worksheet._getPPIX());
+        var wb = this.worksheet && this.worksheet.workbook;
+        wb.setOleSize(null);
+        var drawingCtx = AscCommonExcel.getContext(this.ext.cx * koef, this.ext.cy * koef, wb);
+        var graphics = AscCommonExcel.getGraphics(drawingCtx);
+        this.draw(graphics);
+        return drawingCtx.toDataURL();
+    };
+
     DrawingBase.prototype.getAllFonts = function(AllFonts) {
         var _t = this;
         _t.graphicObject && _t.graphicObject.documentGetAllFontNames && _t.graphicObject.documentGetAllFontNames(AllFonts);
--- common/Drawings/Format/ChartSpace.js
+++ common/Drawings/Format/ChartSpace.js
@@ -8024,16 +8024,7 @@ var GLOBAL_PATH_COUNT = 0;
         }
     };
     CChartSpace.prototype.createImage = function () {
-        var nCoefficient = Asc.getCvtRatio(3, 0, this.drawingBase.worksheet._getPPIX());
-        var oWorkbook = this.drawingBase.worksheet && this.drawingBase.worksheet.workbook;
-        oWorkbook.setOleSize(null);
-        var oDrawingContext = AscCommonExcel.getContext(this.extX * nCoefficient, this.extY * nCoefficient, oWorkbook);
-        var oGraphics = AscCommonExcel.getGraphics(oDrawingContext);
-        const oOldTransform = this.transform.CreateDublicate();
-        this.transform.Reset();
-        this.draw(oGraphics);
-        this.transform = oOldTransform;
-        return oDrawingContext.toDataURL();
+        return this.drawingBase.createImage();
     }
     CChartSpace.prototype.checkDrawingCache = function(graphics) {
         if(window["NATIVE_EDITOR_ENJINE"] || graphics.RENDERER_PDF_FLAG || this.isSparkline || this.bPreview || graphics.PrintPreview) {
