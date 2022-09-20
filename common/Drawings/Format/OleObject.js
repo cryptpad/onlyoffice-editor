6103da5c1a50f77fdfa9fc28466d4b298284be12
Vladimir Privezenov <privezenoff@yandex.ru>
2022-09-20 17:56:49 +0300

    Revert "Fix lock object with editing ole object in right panel"
    This reverts commit d27aa90dfdd8938c31401656c7994b27ae6fdfbd.

--- common/Drawings/Format/OleObject.js
+++ common/Drawings/Format/OleObject.js
@@ -606,17 +606,6 @@ function (window, undefined) {
     function asc_putBinaryDataToFrameFromTableOleObject(oOleObject)
     {
         if (oOleObject instanceof AscFormat.COleObject) {
-            const oApi = Asc.editor || editor;
-            if (!oApi.isOpenedChartFrame) {
-                oApi.isOleEditor = true;
-                oApi.asc_onOpenChartFrame();
-                const oController = oApi.getGraphicController();
-                if (oController) {
-                    AscFormat.ExecuteNoHistory(function () {
-                        oController.checkSelectedObjectsAndCallback(function () {}, [], false);
-                    }, this, []);
-                }
-            }
             const nDataSize = oOleObject.m_aBinaryData.length;
             const sData = AscCommon.Base64.encode(oOleObject.m_aBinaryData);
             const nImageWidth = oOleObject.extX * AscCommon.g_dKoef_mm_to_pix;
