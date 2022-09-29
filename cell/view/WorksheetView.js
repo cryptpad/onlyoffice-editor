c431cc28edc7c17f043000ff673101cfb2b4ed7b
GoshaZotov <Igor.Zotov@onlyoffice.com>
2022-09-29 13:52:19 +0300

    [se] Fix bug 58112

--- cell/view/WorksheetView.js
+++ cell/view/WorksheetView.js
@@ -9166,7 +9166,7 @@
         }
 
         if (ar.c2 === ar.c1) {
-            if (0 === this._getColumnWidth(ar.c1)) {
+            if (0 !== dc && 0 === this._getColumnWidth(ar.c1)) {
                 c1 = c2 = this._findVisibleCol(ar.c1, dc);
             }
         } else {
@@ -9188,7 +9188,7 @@
         }
 
         if (ar.r2 === ar.r1) {
-            if (0 === this._getRowHeight(ar.r1)) {
+            if (0 !== dr && 0 === this._getRowHeight(ar.r1)) {
                 r1 = r2 = this._findVisibleRow(ar.r1, dr);
             }
         } else {
