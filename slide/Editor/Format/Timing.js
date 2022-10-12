42f7d46ee7bfc1173ec4fda67b0919d4557cbb00
Sergey Luzyanin <Sergey.Luzyanin@onlyoffice.com>
2022-10-12 19:46:36 +0300

    [bug] fix bug 59355

--- slide/Editor/Format/Timing.js
+++ slide/Editor/Format/Timing.js
@@ -2776,18 +2776,24 @@
         }
         var aEffectsForDraw = this.getEffectsForLabelsDraw();
 
-        var oContext = oGraphics.m_oContext;
-        var sOldFill;
-        if (oContext) {
-            var dPR = AscCommon.AscBrowser.retinaPixelRatio;
-            oContext.font = Math.round(8 * dPR) + "pt Arial";
-            oContext.textAlign = "center";
-        }
-        for (var nEffect = 0; nEffect < aEffectsForDraw.length; ++nEffect) {
-            aEffectsForDraw[nEffect].drawEffectLabel(oGraphics);
-        }
-        if (oContext) {
-            oContext.fillStyle = sOldFill;
+        if(aEffectsForDraw.length > 0) {
+            oGraphics.SaveGrState();
+            oGraphics.transform3(new AscCommon.CMatrix());
+            oGraphics.SetIntegerGrid(true);
+            var oContext = oGraphics.m_oContext;
+            var sOldFill;
+            if (oContext) {
+                var dPR = AscCommon.AscBrowser.retinaPixelRatio;
+                oContext.font = Math.round(8 * dPR) + "pt Arial";
+                oContext.textAlign = "center";
+            }
+            for (var nEffect = 0; nEffect < aEffectsForDraw.length; ++nEffect) {
+                aEffectsForDraw[nEffect].drawEffectLabel(oGraphics);
+            }
+            if (oContext) {
+                oContext.fillStyle = sOldFill;
+            }
+            oGraphics.RestoreGrState();
         }
     };
     CTiming.prototype.isDrawAnimLabels = function () {
