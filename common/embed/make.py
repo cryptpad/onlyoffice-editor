859635523b465c77bfb8c7e8a98d45f8afc2dbf7
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2022-10-14 14:46:05 +0300

    Refactoring api.js correction (#3042)

--- common/embed/make.py
+++ common/embed/make.py
@@ -19,19 +19,18 @@ if (1 != len(params)):
 api_file = params[0]
 api_content = base.readFile(api_file)
 
+pos_return_editor_embed = api_content.find("var onMouseUp")
 pos_return_editor_obj = api_content.find("return {")
 
 new_content = ""
-new_content += min_content
-new_content += "\n        "
-new_content += "function _createEmbedWorker() { return new AscEmbed.initWorker(iframe); }"
+new_content += "function _createEmbedWorker() { return AscEmbed.initWorker(iframe); }"
 new_content += "\n\n"
 
 new_content += "        return {"
 new_content += "\n"
 new_content += "            createEmbedWorker   : _createEmbedWorker,"
 
-new_api_content = api_content[0:pos_return_editor_obj] + new_content + api_content[pos_return_editor_obj + 8:]
+new_api_content = api_content[0:pos_return_editor_embed] + min_content + "\n        " + api_content[pos_return_editor_embed:pos_return_editor_obj] + new_content + api_content[pos_return_editor_obj + 8:]
 
 base.delete_file(api_file)
 base.writeFile(api_file, new_api_content)
