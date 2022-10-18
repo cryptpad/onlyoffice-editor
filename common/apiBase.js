ef6e3a8c260e48e16ee97892ac6f90eb51129abf
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2022-10-18 16:47:39 +0300

    Fix bug 59414

--- common/apiBase.js
+++ common/apiBase.js
@@ -3451,6 +3451,9 @@
 		if (!document.activeElement || !document.createEvent || (document.activeElement.id === "area_id"))
 			return;
 
+		if (!document.activeElement.dispatchEvent)
+			return;
+
 		var e = document.createEvent("HTMLEvents");
 		e.initEvent("blur", true, true);
 		e.eventName = "blur";
