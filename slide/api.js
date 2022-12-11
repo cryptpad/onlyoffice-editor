a574344920d68391725a3269b518fc5c1e839353
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2022-12-11 22:41:41 +0300

    Fix bug 60102

--- slide/api.js
+++ slide/api.js
@@ -8476,6 +8476,8 @@ background-repeat: no-repeat;\
 	// print-preview
 	asc_docs_api.prototype.asc_initPrintPreview = function(containerId, options)
 	{
+		if (this.printPreview)
+			return;
 		this.printPreview = new AscCommon.CPrintPreview(this, containerId);
 	};
 	asc_docs_api.prototype.asc_drawPrintPreview = function(index, paperSize)
