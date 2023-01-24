5a3a9af863703dd274f02ce17db5e29f81f882e1
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2023-01-24 23:36:09 +0300

    Fix bug 60822

--- word/api.js
+++ word/api.js
@@ -12475,40 +12475,43 @@ background-repeat: no-repeat;\
 		var oCopyProcessor = new AscCommon.CopyProcessor(this);
 		oCopyProcessor.Start();
 		var _ret = "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />";
-		if (oCore.asc_getTitle())
+		if (oCore)
 		{
-			var title = AscCommon.escapeHtmlCharacters(oCore.asc_getTitle());
-			_ret += ("<title>" + title + "</title>");
-		}
-		if (oCore.asc_getCreator())
-		{
-			var creator = AscCommon.escapeHtmlCharacters(oCore.asc_getCreator());
-			_ret += ("<meta name=\"creator\" content=\"" + creator + "\" />");
-		}
-		if (oCore.asc_getDescription())
-		{
-			var description = AscCommon.escapeHtmlCharacters(oCore.asc_getDescription());
-			_ret += ("<meta name=\"description\" content=\"" + description + "\" />");
-		}
-		if (oCore.asc_getKeywords())
-		{
-			var keywords = AscCommon.escapeHtmlCharacters(oCore.asc_getKeywords());
-			_ret += ("<meta name=\"keywords\" content=\"" + keywords + "\" />");
-		}
-		if (oCore.asc_getSubject())
-		{
-			var subject = AscCommon.escapeHtmlCharacters(oCore.asc_getSubject());
-			_ret += ("<meta name=\"subject\" content=\"" + subject + "\" />");
-		}
-		if (oCore.asc_getIdentifier())
-		{
-			var identifier = AscCommon.escapeHtmlCharacters(oCore.asc_getIdentifier());
-			_ret += ("<meta name=\"identifier\" content=\"" + identifier + "\" />");
-		}
-		if (oCore.asc_getLanguage())
-		{
-			var language = AscCommon.escapeHtmlCharacters(oCore.asc_getLanguage());
-			_ret += ("<meta name=\"language\" content=\"" + language + "\" />");
+			if (oCore.asc_getTitle())
+			{
+				var title = AscCommon.escapeHtmlCharacters(oCore.asc_getTitle());
+				_ret += ("<title>" + title + "</title>");
+			}
+			if (oCore.asc_getCreator())
+			{
+				var creator = AscCommon.escapeHtmlCharacters(oCore.asc_getCreator());
+				_ret += ("<meta name=\"creator\" content=\"" + creator + "\" />");
+			}
+			if (oCore.asc_getDescription())
+			{
+				var description = AscCommon.escapeHtmlCharacters(oCore.asc_getDescription());
+				_ret += ("<meta name=\"description\" content=\"" + description + "\" />");
+			}
+			if (oCore.asc_getKeywords())
+			{
+				var keywords = AscCommon.escapeHtmlCharacters(oCore.asc_getKeywords());
+				_ret += ("<meta name=\"keywords\" content=\"" + keywords + "\" />");
+			}
+			if (oCore.asc_getSubject())
+			{
+				var subject = AscCommon.escapeHtmlCharacters(oCore.asc_getSubject());
+				_ret += ("<meta name=\"subject\" content=\"" + subject + "\" />");
+			}
+			if (oCore.asc_getIdentifier())
+			{
+				var identifier = AscCommon.escapeHtmlCharacters(oCore.asc_getIdentifier());
+				_ret += ("<meta name=\"identifier\" content=\"" + identifier + "\" />");
+			}
+			if (oCore.asc_getLanguage())
+			{
+				var language = AscCommon.escapeHtmlCharacters(oCore.asc_getLanguage());
+				_ret += ("<meta name=\"language\" content=\"" + language + "\" />");
+			}
 		}
 		_ret += ("</head><body>" + oCopyProcessor.getInnerHtml() + "</body></html>");
 
