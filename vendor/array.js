75ed68475b94e6269ea6ad3f72836ca1090f9750
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2023-02-13 21:52:21 +0300

    Refactoring

--- vendor/array.js
+++ vendor/array.js
@@ -1,5 +1,33 @@
 (function(){
+
+	if (!Array.prototype.findIndex)
+	{
+		Object.defineProperty(Array.prototype, 'findIndex', {
+			value: function(predicate) {
+				if (this == null) {
+					throw new TypeError('Array.prototype.findIndex called on null or undefined');
+				}
+				if (typeof predicate !== 'function') {
+					throw new TypeError('predicate must be a function');
+				}
+				var list = Object(this);
+				var length = list.length >>> 0;
+				var thisArg = arguments[1];
+				var value;
+
+				for (var i = 0; i < length; i++) {
+					value = list[i];
+					if (predicate.call(thisArg, value, i, list)) {
+						return i;
+					}
+				}
+				return -1;
+			}
+		});
+	}
+
 	var typed_arrays = [
+		Array,
 		Int8Array,
 		Uint8Array,
 		Int16Array,
