c55691b3f586adeb5aae882a56ab21a9e34c82e9
KirillovIlya <ilya.kirillov@onlyoffice.com>
2023-02-25 14:13:53 +0500

    [all] Fix updating other user cursor for obfuscated version

--- common/CollaborativeEditingBase.js
+++ common/CollaborativeEditingBase.js
@@ -899,9 +899,9 @@
 		if (!info)
 			return;
 		
-		let userId      = info["UserId"];
-		let cursorInfo  = info["CursorInfo"];
-		let shortUserId = info["UserShortId"];
+		let userId      = info.UserId ? info.UserId : info["UserId"];
+		let cursorInfo  = info.CursorInfo ? info.CursorInfo : info["CursorInfo"];
+		let shortUserId = info.UserShortId ? info.UserShortId : info["UserShortId"];
 		
 		if (!userId || !cursorInfo || !shortUserId)
 			return;
