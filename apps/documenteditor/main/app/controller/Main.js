9a9f6b08f78f22801133d124a1ee21f854602854
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2023-04-05 10:43:00 +0200

    Hide "Protect" tab in text and sheets editor

--- apps/documenteditor/main/app/controller/Main.js
+++ apps/documenteditor/main/app/controller/Main.js
@@ -510,6 +510,9 @@ define([
                 if (data.doc) {
                     appHeader.setDocumentCaption(data.doc.title);
                 }
+
+                // XXX CryptPad: disable protect feature
+                this.permissions.protect = false;
             },
 
             onProcessSaveResult: function(data) {
