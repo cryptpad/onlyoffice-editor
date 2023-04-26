c60fe769a6dad92b6f723e1b4899f0f81fb845a3
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2023-04-26 11:08:41 +0200

    Disable external links in sheets

--- apps/spreadsheeteditor/main/app/view/DataTab.js
+++ apps/spreadsheeteditor/main/app/view/DataTab.js
@@ -103,9 +103,10 @@ define([
                 me.fireEvent('data:sortcustom');
             });
 
-            me.btnExternalLinks.on('click', function (b, e) {
-                me.fireEvent('data:externallinks');
-            });
+            // XXX Cryptpad: Disable external links
+            // me.btnExternalLinks.on('click', function (b, e) {
+            //     me.fireEvent('data:externallinks');
+            // });
 
             me.btnDataFromText.menu ?
             me.btnDataFromText.menu.on('item:click', function (menu, item, e) {
@@ -246,18 +247,19 @@ define([
                 });
                 this.lockedControls.push(this.btnCustomSort);
 
-                this.btnExternalLinks = new Common.UI.Button({
-                    parentEl: $host.find('#slot-btn-data-external-links'),
-                    cls: 'btn-toolbar x-huge icon-top',
-                    iconCls: 'toolbar__icon edit-links',
-                    caption: this.capDataExternalLinks,
-                    disabled: true,
-                    lock: [_set.editCell, _set.sheetLock, _set.wbLock, _set.lostConnect, _set.coAuth, _set.wsLock],
-                    dataHint: '1',
-                    dataHintDirection: 'bottom',
-                    dataHintOffset: 'small'
-                });
-                this.lockedControls.push(this.btnExternalLinks);
+                // XXX Cryptpad: Disable external links
+                // this.btnExternalLinks = new Common.UI.Button({
+                //     parentEl: $host.find('#slot-btn-data-external-links'),
+                //     cls: 'btn-toolbar x-huge icon-top',
+                //     iconCls: 'toolbar__icon edit-links',
+                //     caption: this.capDataExternalLinks,
+                //     disabled: true,
+                //     lock: [_set.editCell, _set.sheetLock, _set.wbLock, _set.lostConnect, _set.coAuth, _set.wsLock],
+                //     dataHint: '1',
+                //     dataHintDirection: 'bottom',
+                //     dataHintOffset: 'small'
+                // });
+                // this.lockedControls.push(this.btnExternalLinks);
 
                 this.btnsSortDown = Common.Utils.injectButtons($host.find('.slot-sortdesc'), '', 'toolbar__icon btn-sort-down', '',
                     [_set.editCell, _set.selChart, _set.selChartText, _set.selShape, _set.selShapeText, _set.selImage, _set.lostConnect, _set.coAuth, _set.ruleFilter, _set.cantModifyFilter, _set.sheetLock, _set.cantSort, _set['Sort']], undefined, undefined, undefined, '1', 'top', undefined, 'D');
@@ -325,7 +327,8 @@ define([
                     me.btnTextToColumns.updateHint(me.tipToColumns);
                     me.btnRemoveDuplicates.updateHint(me.tipRemDuplicates);
                     me.btnDataValidation.updateHint(me.tipDataValidation);
-                    me.btnExternalLinks.updateHint(me.tipExternalLinks);
+                    // XXX Cryptpad: Disable external links
+                    // me.btnExternalLinks.updateHint(me.tipExternalLinks);
 
                     me.btnsSortDown.forEach( function(btn) {
                         btn.updateHint(me.toolbar.txtSortAZ);
