11816b3a154e6a00f429d6bb7bd4658bec60280d
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2024-08-02 10:35:06 +0200

    Start and stop docker container for e2e tests
    https://github.com/cryptpad/onlyoffice-editor/issues/2
    https://github.com/cryptpad/onlyoffice-editor/issues/3

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 ```
 bazel run -- @pnpm//:pnpm --dir $PWD install --lockfile-only
 ```
+
+# Running all tests
+
+```
+bazel test --test_output=errors //...
+```
--- e2etests/test.spec.js
+++ e2etests/test.spec.js
@@ -1,9 +1,28 @@
-describe("A suite is just a function", function() {
-  let a;
+import { GenericContainer } from "testcontainers";
+import fetch from 'node-fetch';
 
-  it("and so is a spec", function() {
-    a = false;
+const DOCKER_START_TIMEOUT_MS = 1 * 60 * 1000;
 
-    expect(a).toBe(true);
-  });
+describe("A suite is just a function", function () {
+    let container;
+    let httpPort;
+
+    beforeAll(async () => {
+        container = await new GenericContainer("cp-documentserver:latest")
+            .withExposedPorts(80)
+            .start();
+
+        httpPort = container.getFirstMappedPort();
+    }, DOCKER_START_TIMEOUT_MS);
+
+    afterAll(async () => {
+        await container.stop();
+    });
+
+    it("and so is a spec", async function () {
+        const url = new URL("http://localhost/welcome/");
+        url.port = httpPort;
+        const response = await fetch(url);
+        expect(response.status).toBe(200);
+    });
 });
