8c06f735a873596d5c26340489f72c9a92aaa12f
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2024-08-29 18:36:57 +0200

    Test welcome page

--- e2etests/playwright-test.mjs
+++ e2etests/playwright-test.mjs
@@ -1,17 +1,48 @@
-import { execSync } from 'child_process';
-import { join } from 'path';
-import { readdirSync } from 'fs';  // TODO clean up debug logs
+import { promisify } from 'node:util';
+import { join } from 'node:path';
+import assert from 'node:assert/strict';
+import { GenericContainer } from "testcontainers";
+import { spawnSync } from 'node:child_process';
 
-const browserCacheRelative = process.env.BROWSERS_PATH;
-const playwrightCommand = process.argv[2];
-console.log('XXX', { argv: process.argv, browserCacheRelative, playwrightCommand });
-const browserCache = join(process.cwd(), 'e2etests', 'ms-playwright');
-console.log(readdirSync(browserCache));
+async function main() {
+    let httpPort;
+    const playwrightCommand = process.argv[2];
+    const browserCache = join(process.cwd(), 'e2etests', 'ms-playwright');
 
-console.log('XXX', { argv: process.argv, browserCacheRelative, browserCache, playwrightCommand });
+    process.chdir('e2etests');
 
-process.chdir('e2etests');
-execSync(`npm  --cache . exec -- playwright ${playwrightCommand}`, {
-    stdio: 'inherit',
-    env: { ...process.env, PLAYWRIGHT_BROWSERS_PATH: browserCache }
+    const dockerInfo = spawnSync("docker", ["load", "-i", "../docker/load/tarball.tar"], {
+        stdio: "inherit",
+    });
+    assert.equal(dockerInfo.status, 0);
+
+    const container = await new GenericContainer("cp-documentserver:latest")
+        .withExposedPorts({
+            container: 80,
+            host: 80
+        })
+        .start();
+
+    try {
+        httpPort = container.getFirstMappedPort();
+        const { output, exitCode } = await container.exec([
+            "supervisorctl",
+            "start",
+            "ds:example",
+        ]);
+        assert.equal(exitCode, 0)
+
+        const npmInfo = spawnSync('npm', ['--cache', '.', 'exec', '--', 'playwright', playwrightCommand], {
+            stdio: 'inherit',
+            env: { ...process.env, PLAYWRIGHT_BROWSERS_PATH: browserCache }
+        });
+        assert.equal(npmInfo.status, 0);
+    } finally {
+        await container.stop();
+    }
+}
+
+main().catch((e) => {
+    console.error(e);
+    process.exit(1);
 });
--- e2etests/tests/example.spec.js
+++ e2etests/tests/example.spec.js
@@ -1,19 +1,8 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('welcome page', async ({ page }) => {
+    await page.goto('http://localhost/welcome/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright XXX/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    await expect(page.getByText("GO TO TEST EXAMPLE")).toBeVisible();
 });
